8f6b2dd6e0b02fcac94c4ddccdab04ca8cea45d8
Jacques Dafflon <mail@0xjac.com>
2019-02-18 00:03:52 +0100

    ERC820: Supersed ERC820 with ERC820a
    - Mark ERC820 as superseded
    - Add ERC820a and set to alst call
    - Adapt travis validation script to accept ERC820a
    - change Jacques' email due to a bug with github search (which made the
      previous email unfindable)

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -15,13 +15,13 @@ elif [[ $TASK = 'htmlproofer-external' ]]; then
   bundle exec jekyll build
   bundle exec htmlproofer $HTMLPROOFER_OPTIONS --external_only
 elif [[ $TASK = 'eip-validator' ]]; then
-  BAD_FILES="$(ls EIPS | egrep -v "eip-[0-9]+.md|eip-20-token-standard.md")" || true
+  BAD_FILES="$(ls EIPS | egrep -v "eip-[0-9]+.md|eip-20-token-standard.md|eip-820a.md")" || true
   if [[ ! -z $BAD_FILES ]]; then
     echo "Files found with invalid names:"
     echo $BAD_FILES
     exit 1
   fi
 
-  FILES="$(ls EIPS/*.md | egrep "eip-[0-9]+.md")"
+  FILES="$(ls EIPS/*.md | egrep "eip-[0-9]+.md|eip-820a.md")"
   bundle exec eip_validator $FILES
 fi
