6aba39ce0f8fe5285650f8c65e83c4f548e7b3b0
alita-moore <alita.moore805@gmail.com>
2021-02-15 16:18:47 -0600

    complete python -> javascript translation (non-functional)

--- .github/actions/index.ts
+++ .github/actions/index.ts
@@ -1,5 +1,6 @@
 import core from "@actions/core";
-import github from "@actions/github";
+import {context, getOctokit} from "@actions/github";
+import frontmatter from "front-matter";
 // import Github from "github-api";
 
 try {
@@ -27,7 +28,7 @@ try {
 // import re
 // import webapp2
 
-const config = {"GITHUB_ACCESS_TOKEN": "placeholder"};
+const config = { GITHUB_ACCESS_TOKEN: "placeholder" };
 
 const FILE_RE = new RegExp("^EIPS/eip-(d+).md$");
 const AUTHOR_RE = new RegExp("[(<]([^>)]+)[>)]");
@@ -38,161 +39,273 @@ Hi, I'm a bot! This change was automatically merged because:
  - The build is passing
 `;
 
-const Github = github.getOctokit(config.GITHUB_ACCESS_TOKEN)
+const Github = getOctokit(config.GITHUB_ACCESS_TOKEN);
 
-const EIPInfo = {'EIPInfo': ['number', 'authors']};
+type UnPromisify<T> = T extends Promise<infer U> ? U : T;
+type PR = UnPromisify<ReturnType<typeof Github.pulls.get>>;
+
+type CompareCommits = UnPromisify<ReturnType<typeof Github.repos.compareCommits>>;
+var _compared_: CompareCommits;
+type Files = typeof _compared_.data.files;
+var _files_: Files;
+type File = typeof _files_[0];
+
+
+const EIPInfo = { EIPInfo: ["number", "authors"] };
 
 let users_by_email = {};
 
 const find_user_by_email = (email: string) => {
-  if ( !users_by_email[email] ) {
-    const results = Github.search_users(email)
-    if ( results.length > 0) {
-      console.log("Recording mapping from %s to %s", email, results[0].login)
-      users_by_email[email] = '@' + results[0].login
+  if (!users_by_email[email]) {
+    const results = Github.search_users(email);
+    if (results.length > 0) {
+      console.log("Recording mapping from %s to %s", email, results[0].login);
+      users_by_email[email] = "@" + results[0].login;
     } else {
-      console.log("No github user found for %s", email)
+      console.log("No github user found for %s", email);
     }
-  } else return users_by_email[email]
-}
+  } else return users_by_email[email];
+};
 
-const ALLOWED_STATUSES = new Set(['draft', 'last call', 'review'])
+const ALLOWED_STATUSES = new Set(["draft", "last call", "review"]);
+
+const request = await Github.repos.compareCommits({
+  base: context.payload.pull_request?.base?.sha,
+  head: context.payload.pull_request?.head?.sha,
+  owner: context.repo.owner,
+  repo: context.repo.repo
+})
 
 // class MergeHandler(webapp2.RequestHandler):
-  const resolve_author = (author: string) => {
-    if (author[0] === '@') {
-      return author.toLowerCase()
-    } else {
-      // Email address
-      return (find_user_by_email(author) || author).toLowerCase()
+
+const resolve_author = (author: string) => {
+  if (author[0] === "@") {
+    return author.toLowerCase();
+  } else {
+    // Email address
+    return (find_user_by_email(author) || author).toLowerCase();
+  }
+};
+
+const get_authors = (authorlist: string[]) => {
+  const authors = authorlist.map((author) => author.match(AUTHOR_RE));
+  return new Set(authors.map(resolve_author));
+};
+
+const check_file = ({data: pr}: PR, file: File) => {
+  const fileName: string = file.filename;
+  
+  try {
+    const match = fileName.search(FILE_RE);
+    if (!match) {
+      return [null, `File ${fileName} is not an EIP`];
+    }
+
+    const eipnum = match[0];
+
+    if (file.status == "added") {
+      return [null, `Contains new file ${fileName}`];
+    }
+
+    Github.log.info(
+      `Getting file ${fileName} from ${pr.base.user.login}@${pr.base.repo.name}/${pr.base.sha}`
+    );
+    const base = pr.base.repo.get_contents(file.filename, pr.base.sha);
+    const basedata = frontmatter(btoa(base.content));
+
+    const status = basedata.attributes["status"];
+    const author = basedata.attributes["author"];
+    if (ALLOWED_STATUSES.has(status.toLowerCase())) {
+      return [
+        null,
+        `EIP ${eipnum} is in state ${status}, not Draft or Last Call`,
+      ];
+    }
+    const eip = EIPInfo(eipnum, get_authors(author));
+
+    if (basedata.attributes["eip"] !== eipnum) {
+      return [
+        eip,
+        `EIP header in ${fileName} does not match: ${basedata.attributes["eip"]}`,
+      ];
+    }
+
+    Github.log.info(
+      "`Getting file ${fileName} from ${pr.base.user.login}@${pr.base.repo.name}/${pr.base.sha}`"
+    );
+    const head = pr.head.repo.get_contents(file.filename, pr.head.sha); // ref=pr.head.sha
+    const headdata = frontmatter(btoa(head.content));
+    if (headdata.attributes["eip"] != eipnum) {
+      return [
+        eip,
+        `EIP header in modified file ${fileName} does not match: ${headdata.attributes["eip"]}`,
+      ];
+    } else if (
+      headdata.attributes["status"].toLowerCase() !=
+      basedata.attributes["status"].toLowerCase()
+    ) {
+      return [
+        eip,
+        `Trying to change EIP ${eipnum} state from ${basedata.attributes["status"]} to ${headdata.attributes["status"]}`,
+      ];
+    }
+
+    return [eip, null];
+  } catch (e) {
+    console.error("Exception checking file %s", file.filename);
+    return [null, `Error checking file ${file.filename}`];
+  }
+};
+
+const post = () => {
+  const payload = JSON.parse(request["payload"]);
+  if (request.headers.includes("X-Github-Event")) {
+    
+    const event = request.headers["X-Github-Event"];
+    Github.log.info(`Got Github webhook event ${event}`);
+    if (event == "pull_request_review") {
+      const pr = payload["pull_request"];
+      const prnum = pr["number"];
+      const repo = pr["base"]["repo"]["full_name"];
+      Github.log.info("Processing review on PR ${repo}/${prnum}...");
+      check_pr(repo, prnum);
+    }
+  } else {
+    Github.log.info(`Processing build ${payload["number"]}...`);
+    if (payload["pull_request_number"] === null) {
+      Github.log.info(
+        "Build %s is not a PR build; quitting",
+        payload["number"]
+      );
+      return;
+    }
+    const prnum = payload["pull_request_number"];
+    const repo = `${payload["repository"]["owner_name"]}/${payload["repository"]["name"]}`;
+    check_pr(repo, prnum);
+  }
+};
+
+const get = () => {
+  return check_pr(request["repo"], JSON.parse(request["pr"]))
+}
+
+const get_approvals = (pr) => {
+  let approvals = '@' + pr.user.login.toLowerCase()
+  const reviews = pr.get_reviews()
+
+  reviews.map(review => {
+    if (review.state == "APPROVED") {
+      approvals += '@' + review.user.login.toLowerCase()
+    }
+  })
+      
+  return approvals
+}
+
+const post_comment = async (pr: PR, message: string) => {
+  const me = Github.get_user()
+  const {data: comments} = await Github.issues.listComments();
+
+  // If comment already exists, edit that 
+  for (const comment of comments) {
+    if (comment.user.login == me.login) {
+      Github.log.info("Found comment by self");
+    } 
+    
+    if (comment.body != message) {
+      Github.issues.updateComment({
+        owner: comment.user.login,
+        repo: pr.data.base.repo.full_name,
+        comment_id: comment.id,
+        body: message
+      })
+      return;
     }
   }
+  
+  // if comment does not exist, create a new one
+  Github.issues.createComment({
+    owner: pr.data.base.repo.owner.login,
+    repo: pr.data.base.repo.full_name,
+    issue_number: pr.data.number,
+    body: message
+  })
+}
 
-  const get_authors = (authorlist: string[]) => {
-    const authors = authorlist.map(author => author.match(AUTHOR_RE));
-    return new Set(authors.map(resolve_author))
-  }
+const check_pr = async (reponame, prnum) => {
+    Github.log.info(`Checking PR ${prnum} on ${reponame}`)
+    const repos = await Github.search.repos(reponame);
+    const repo = repos.data.items.find(repo => repo.name === reponame);
+    const pr = await Github.pulls.get({repo: repo.full_name, owner: repo.owner.login, pull_number: prnum})
+    let response = "";
 
-    // def check_file(self, pr, file):
-    //     try:
-    //         match = FILE_RE.search(file.filename)
-    //         if not match:
-    //             return (None, "File %s is not an EIP" % (file.filename,))
-    //         eipnum = int(match.group(1))
+    if ( pr.data.merged ) {
+      Github.log.info("PR %d is already merged; quitting", prnum)
+      return;
+    }
+    if (pr.data.mergeable_state != 'clean') {
+      Github.log.info(`PR ${prnum} mergeable state is ${pr.data.mergeable_state}; quitting`)
+      return;
+    }
 
-    //         if file.status == "added":
-    //             return (None, "Contains new file %s" % (file.filename,))
+    const files = request.data.files;
 
-    //         logging.info("Getting file %s from %s@%s/%s", file.filename, pr.base.user.login, pr.base.repo.name, pr.base.sha)
-    //         base = pr.base.repo.get_contents(file.filename, ref=pr.base.sha)
-    //         basedata = frontmatter.loads(base64.b64decode(base.content))
-    //         if basedata.get("status").lower() not in ALLOWED_STATUSES:
-    //             return (None, "EIP %d is in state %s, not Draft or Last Call" % (eipnum, basedata.get("status")))
+    let eips = []
+    let errors = []
+    files.map(file => {
+      const [eip, error] = check_file(pr, file)
+      if (eip){
+          eips.push(eip)
+      }
+      if (error) {
+        Github.log.info(error)
+        errors.push(error)
+      }
+    })
 
-    //         eip = EIPInfo(eipnum, self.get_authors(basedata.get("author")))
+    let reviewers = new Set()
+    const approvals = get_approvals(pr)
+    Github.log.info(`Found approvals for ${prnum}: ${approvals}`)
+    // TODO: define eip types
+    eips.map(eip => {
+      const authors: Set<string> = eip.authors;
+      const number: string = eip.number;
+      Github.log.info(`EIP ${number} has authors: ${authors}`);
+      if (authors.size == 0) {
+        errors.push(`EIP ${number} has no identifiable authors who can approve PRs`)
+      } else if ([...approvals].find(authors.has)){
+        errors.push(`EIP ${number} requires approval from one of (${authors})`);
+        [...authors].map(author => {
+          if (author.startsWith('@')) {
+            reviewers.add(author.slice(1))
+          }
+        })
+      }
+    })
+        
 
-    //         if basedata.get("eip") != eipnum:
-    //             return (eip, "EIP header in %s does not match: %s" % (file.filename, basedata.get("eip")))
+    if (errors.length === 0){
+      Github.log.info(`Merging PR ${prnum}!`);
+      response = `Merging PR ${prnum}!`;
 
-    //         logging.info("Getting file %s from %s@%s/%s", file.filename, pr.head.user.login, pr.head.repo.name, pr.head.sha)
-    //         head = pr.head.repo.get_contents(file.filename, ref=pr.head.sha)
-    //         headdata = frontmatter.loads(base64.b64decode(head.content))
-    //         if headdata.get("eip") != eipnum:
-    //             return (eip, "EIP header in modified file %s does not match: %s" % (file.filename, headdata.get("eip")))
-    //         if headdata.get("status").lower() != basedata.get("status").lower():
-    //             return (eip, "Trying to change EIP %d state from %s to %s" % (eipnum, basedata.get("status"), headdata.get("status")))
+      const eipNumbers = eips.join(', ');
+      Github.pulls.merge({
+        pull_number: pr.data.number,
+        repo: pr.data.base.repo.full_name,
+        owner: pr.data.base.repo.owner.login,
+        commit_title: `Automatically merged updates to draft EIP(s) ${eipNumbers} (#${prnum})`,
+        commit_message: MERGE_MESSAGE,
+        merge_method: "squash",
+        sha: pr.data.head.sha
+      })
+    } else if (errors.length > 0 && eips.length > 0) {
+      let message = "Hi! I'm a bot, and I wanted to automerge your PR, but couldn't because of the following issue(s):\n\n"
+      message += errors.join("\n - ");
 
-    //         return (eip, None)
-    //     except Exception, e:
-    //         logging.exception("Exception checking file %s", file.filename)
-    //         return (None, "Error checking file %s" % (file.filename,))
+      post_comment(pr, message)
+    }
 
-    // def post(self):
-    //     payload = json.loads(self.request.get("payload"))
-    //     if 'X-Github-Event' in self.request.headers:
-    //         event = self.request.headers['X-Github-Event']
-    //         logging.info("Got Github webhook event %s", event)
-    //         if event == "pull_request_review":
-    //             pr = payload["pull_request"]
-    //             prnum = int(pr["number"])
-    //             repo = pr["base"]["repo"]["full_name"]
-    //             logging.info("Processing review on PR %s/%d...", repo, prnum)
-    //             self.check_pr(repo, prnum)
-    //     else:
-    //         logging.info("Processing build %s...", payload["number"])
-    //         if payload.get("pull_request_number") is None:
-    //             logging.info("Build %s is not a PR build; quitting", payload["number"])
-    //             return
-    //         prnum = int(payload["pull_request_number"])
-    //         self.check_pr(payload["repository"]["owner_name"] + "/" + payload["repository"]["name"], prnum)
 
-    // def get(self):
-    //     self.check_pr(self.request.get("repo"), int(self.request.get("pr")))
-
-    // def get_approvals(self, pr):
-    //     approvals = ['@' + pr.user.login.lower()]
-    //     for review in pr.get_reviews():
-    //         if review.state == "APPROVED":
-    //             approvals.append('@' + review.user.login.lower())
-    //     return approvals
-
-    // def check_pr(self, reponame, prnum):
-    //     logging.info("Checking PR %d on %s", prnum, reponame)
-    //     repo = github.get_repo(reponame)
-    //     pr = repo.get_pull(prnum)
-    //     if pr.merged:
-    //         logging.info("PR %d is already merged; quitting", prnum)
-    //         return
-    //     if pr.mergeable_state != 'clean':
-    //         logging.info("PR %d mergeable state is %s; quitting", prnum, pr.mergeable_state)
-    //         return
-
-    //     eips = []
-    //     errors = []
-    //     for file in pr.get_files():
-    //         eip, error = self.check_file(pr, file)
-    //         if eip is not None:
-    //             eips.append(eip)
-    //         if error is not None:
-    //             logging.info(error)
-    //             errors.append(error)
-
-    //     reviewers = set()
-    //     approvals = self.get_approvals(pr)
-    //     logging.info("Found approvals for %d: %r", prnum, approvals)
-    //     for eip in eips:
-    //         logging.info("EIP %d has authors: %r", eip.number, eip.authors)
-    //         if len(eip.authors) == 0:
-    //             errors.append("EIP %d has no identifiable authors who can approve PRs" % (eip.number,))
-    //         elif eip.authors.isdisjoint(approvals):
-    //             errors.append("EIP %d requires approval from one of (%s)" % (eip.number, ', '.join(eip.authors)))
-    //             for author in eip.authors:
-    //                 if author.startswith('@'):
-    //                     reviewers.add(author[1:])
-
-    //     if len(errors) == 0:
-    //         logging.info("Merging PR %d!", prnum)
-    //         self.response.write("Merging PR %d!" % (prnum,))
-    //         pr.merge(
-    //             commit_title="Automatically merged updates to draft EIP(s) %s (#%d)" % (', '.join('%s' % eip.number for eip in eips), prnum),
-    //             commit_message=MERGE_MESSAGE,
-    //             merge_method="squash",
-    //             sha=pr.head.sha)
-    //     elif len(errors) > 0 and len(eips) > 0:
-    //         message = "Hi! I'm a bot, and I wanted to automerge your PR, but couldn't because of the following issue(s):\n\n"
-    //         message += "\n".join(" - " + error for error in errors)
-
-    //         self.post_comment(pr, message)
-
-    // def post_comment(self, pr, message):
-    //     me = github.get_user()
-    //     for comment in pr.get_issue_comments():
-    //         if comment.user.login == me.login:
-    //             logging.info("Found comment by myself")
-    //             if comment.body != message:
-    //                 comment.edit(message)
-    //             return
-    //     pr.create_issue_comment(message)
 
 // app = webapp2.WSGIApplication([
 //     ('/merge/', MergeHandler),
