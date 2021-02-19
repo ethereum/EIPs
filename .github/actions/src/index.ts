7f141a540bc8f74d9dad91d87dcccae7efa7f719
alita-moore <alita.moore805@gmail.com>
2021-02-19 00:44:04 -0600

    check headers and event

--- .github/actions/src/index.ts
+++ .github/actions/src/index.ts
@@ -3,12 +3,6 @@ import { getOctokit, context } from "@actions/github";
 import { Github } from "./types";
 
 const main = async (Github: Github) => {
-  console.log({
-    base: context.payload.pull_request?.base?.sha,
-    head: context.payload.pull_request?.head?.sha,
-    owner: context.repo.owner,
-    repo: context.repo.repo
-  })
   const request = await Github.repos.compareCommits({
     base: context.payload.pull_request?.base?.sha,
     head: context.payload.pull_request?.head?.sha,
@@ -16,35 +10,31 @@ const main = async (Github: Github) => {
     repo: context.repo.repo
   })
 
-  console.log(request);
+  const payload = context.payload;
   
-  const headers = request.headers;
-  console.log(headers);
-  // console.log(headers.type);
-  // if (headers)) {
-    
-  //   const event = request.headers["X-Github-Event"];
-  //   console.log(`Got Github webhook event ${event}`);
-  //   if (event == "pull_request_review") {
-  //     const pr = payload["pull_request"];
-  //     const prnum = pr["number"];
-  //     const repo = pr["base"]["repo"]["full_name"];
-  //     console.log("Processing review on PR ${repo}/${prnum}...");
-  //     check_pr(repo, prnum);
-  //   }
-  // } else {
-  //   console.log(`Processing build ${payload["number"]}...`);
-  //   if (payload["pull_request_number"] === null) {
-  //     console.log(
-  //       "Build %s is not a PR build; quitting",
-  //       payload["number"]
-  //     );
-  //     return;
-  //   }
-  //   const prnum = payload["pull_request_number"];
-  //   const repo = `${payload["repository"]["owner_name"]}/${payload["repository"]["name"]}`;
-  //   check_pr(repo, prnum);
-  // }
+  if (request.headers) {
+    const event = context.eventName;
+    console.log(`Got Github webhook event ${event}`);
+    if (event == "pull_request_review") {
+      const pr = payload.pull_request;
+      const prnum = pr.number;
+      const repo = payload.repository.full_name;
+      console.log(`Processing review on PR ${repo}/${prnum}...`);
+      // check_pr(repo, prnum);
+    }
+  } else {
+    console.log(`Processing build ${payload.sender.type}...`);
+    if (!payload.pull_request.number) {
+      console.log(
+        "Build ?? is not a PR build; quitting"
+      );
+      return;
+    }
+    const prnum = payload.pull_request.number;
+    const repo = `${payload.repository.owner.name}/${payload.repository.name}`;
+    console.log(`prnum: ${prnum}, repo: ${repo}`)
+    // check_pr(repo, prnum);
+  }
 
   console.log(request);
 }
