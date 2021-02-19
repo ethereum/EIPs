217962dddca3cd3bccf3984fe69a621b68f6edef
alita-moore <alita.moore805@gmail.com>
2021-02-18 22:52:46 -0600

    test

--- .github/actions/dist/index.js
+++ .github/actions/dist/index.js
@@ -5802,6 +5802,7 @@ try {
     // `who-to-greet` input defined in action metadata file
     const nameToGreet = (0,_actions_core__WEBPACK_IMPORTED_MODULE_0__.getInput)("who-to-greet");
     console.log(`Hello ${nameToGreet}!`);
+    (0,_actions_github__WEBPACK_IMPORTED_MODULE_1__.getOctokit)(process.env.GITHUB_TOKEN);
     console.log(`token ${process.env.ACTIONS_RUNTIME_TOKEN.length} | ${process.env.GITHUB_TOKEN}`);
     const time = new Date().toTimeString();
     (0,_actions_core__WEBPACK_IMPORTED_MODULE_0__.setOutput)("time", time);
--- .github/actions/index.ts
+++ .github/actions/index.ts
@@ -10,6 +10,7 @@ try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = getInput("who-to-greet");
   console.log(`Hello ${nameToGreet}!`);
+  getOctokit(process.env.GITHUB_TOKEN)
   console.log(`token ${process.env.ACTIONS_RUNTIME_TOKEN.length} | ${process.env.GITHUB_TOKEN}`)
   const time = new Date().toTimeString();
   setOutput("time", time);
