5d6039bdfec4b80272118606a1be9e71490ed4e6
izayl <izayl@163.com>
2021-10-12 21:52:16 +0800

    feat: gen sidebar by type/status/category

--- EIPS/.vuepress/config.js
+++ EIPS/.vuepress/config.js
@@ -1,3 +1,43 @@
+const yamlHeaderParser = require('yaml-front-matter')
+const fs = require('fs')
+const path = require('path')
+const allEipFiles = fs
+  .readdirSync(path.resolve(__dirname, '../'))
+  .filter(file => file.includes('eip'))
+
+const EIP_STATUS = {}
+const EIP_TYPE = {}
+const EIP_CATE = {}
+
+allEipFiles.map(file => {
+  const meta = yamlHeaderParser.loadFront(
+    fs.readFileSync(`${__dirname}/../${file}`, 'utf8')
+  )
+  if (meta.status) {
+    EIP_STATUS[meta.status] = (EIP_STATUS[meta.status] || 0) + 1
+  }
+  if (meta.type) {
+    EIP_TYPE[meta.type] = (EIP_TYPE[meta.type] || 0) + 1
+  }
+  if (meta.category) {
+    EIP_CATE[meta.category] = (EIP_CATE[meta.category] || 0) + 1
+  }
+})
+
+const getSidebarChildren = arr => {
+  return Object.keys(arr).map(item => [
+    `/summary/${item
+      .split(' ')
+      .join('-')
+      .toLowerCase()}`,
+    `${item} (${arr[item]})`,
+  ])
+}
+
+console.log(getSidebarChildren(EIP_STATUS))
+console.log(getSidebarChildren(EIP_TYPE))
+console.log(getSidebarChildren(EIP_CATE))
+
 module.exports = {
   title: '以太坊改进提案 EIPs',
   description:
@@ -22,32 +62,24 @@ module.exports = {
     nav: [{ text: 'About us', link: 'https://ethlibrary.io' }],
     sidebar: [
       {
-        title: 'All EIPs',
-        path: '/all',
+        title: 'All EIPs List',
+        path: '/summary/all',
         collapsable: false,
       },
       {
-        title: 'Status',
+        title: 'EIPs by Status',
         collapsable: false,
-        children: [
-          ['/draft', 'Draft'],
-          ['/review', 'Review'],
-          ['/lastCall', 'Last Call'],
-          ['/accepted', 'Accepted'],
-          ['/final_and_living', 'Final and Living'],
-        ],
+        children: getSidebarChildren(EIP_STATUS),
       },
       {
-        title: 'Type',
+        title: 'EIPs by Types',
         collapsable: false,
-        children: [
-          ['/core', 'Core (165)'],
-          ['/networking', 'Networking (11)'],
-          ['/interface', 'Interface (38)'],
-          ['/erc', 'ERC (153)'],
-          ['/meta', 'Meta (18)'],
-          ['/informational', 'Informational (6)'],
-        ],
+        children: getSidebarChildren(EIP_TYPE),
+      },
+      {
+        title: 'EIPs by Category',
+        collapsable: false,
+        children: getSidebarChildren(EIP_CATE),
       },
     ],
   },
