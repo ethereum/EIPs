4ff8061070aa4e348590ff366b991a3cdc04232a
izayl <izayl@163.com>
2021-12-13 15:01:54 +0800

    feat: add vercel brand

---
+++ EIPS/.vuepress/theme/components/PageNav.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <page-nav v-bind="$props" />
+    <a
+      class="vercel"
+      href="https://vercel.com?utm_source=ethlibrary&utm_campaign=oss"
+      target="_blank"
+    >▲ Powered by Vercel</a>
+  </div>
+</template>
+
+<script>
+import PageNav from '@theme/components/PageNav.vue'
+export default {
+  // components: { PageNav },
+}
+</script>
+
+<style>
+.vercel {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #ccc;
+}
+
+.vercel:hover {
+  color: #000;
+}
+</style>
