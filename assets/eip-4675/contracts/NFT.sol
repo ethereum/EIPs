b3e2b703860f8cde35ab46a5b7d4e5737de8e61a
PowerStream3604 <helloyongjun3604@gmail.com>
2022-06-04 10:59:05 +0900

    update syntax & delete unneeded files

--- assets/eip-4675/contracts/NFT.sol
+++ assets/eip-4675/contracts/NFT.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: ISC
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
--- assets/eip-4675/contracts/math/SafeMath.sol
+++ assets/eip-4675/contracts/math/SafeMath.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: ISC
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 /**
