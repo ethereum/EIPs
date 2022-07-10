d64ef4f760c1a94e010b1557c6cda323999e1f82
in2 <akagawa@synschismo.com>
2022-07-10 23:38:35 +0900

    fix import ERC4907

--- assets/eip-4907/contracts/extensions/ERC4907URIStorage.sol
+++ assets/eip-4907/contracts/extensions/ERC4907URIStorage.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: CC0-1.0
 pragma solidity ^0.8.0; 
 
-import "./ERC4907.sol";
+import "../ERC4907.sol";
 
 abstract contract ERC4907URIStorage is ERC4907 {
     using Strings for uint256;
