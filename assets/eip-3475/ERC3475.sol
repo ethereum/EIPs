f2c6d5a217f681e365854997118786cb18241d53
dhruvmalik007 <malikdhruv1994@gmail.com>
2022-08-01 13:13:54 +0200

    removing reference implementation

--- assets/eip-3475/ERC3475.sol
+++ assets/eip-3475/ERC3475.sol
@@ -2,8 +2,8 @@
 
 pragma solidity ^0.8.0;
 
-import "./IERC3475.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "./interfaces/IERC3475.sol";
+import "./utils/Ownable.sol";
 
 contract ERC3475 is IERC3475, Ownable {
     /** 
