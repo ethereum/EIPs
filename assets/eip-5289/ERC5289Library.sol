c4a0bb6c5a369e15a7419499bc0f2abcd7855e98
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2022-08-05 14:16:11 -0400

    When you remember account abstraction is going to be a thing

--- assets/eip-5289/ERC5289Library.sol
+++ assets/eip-5289/ERC5289Library.sol
@@ -1,38 +1,39 @@
 /// SPDX-License-Identifier: CC0-1.0
 pragma solidity ^0.8.0;
 
-import "./ERC165Storage.sol";
-import "./SignatureChecker.sol";
+import "./ERC5289Library.sol";
+import "./IERC165.sol";
 
-contract ERC5289Library is IERC5289Library, ERC165Storage {
-    uint48 private counter = 0;
-    mapping(uint48 => bytes memory) private multihashes;
-    mapping(uint48 => bytes32) private signatureHashes;
-    mapping(uint48 => mapping(address => bool)) signed;
-    mapping(uint48 => mapping(address => uint64)) signedAt;
+contract ERC5289Library is IERC5289Library, IERC165 {
+    uint16 private counter = 0;
+    mapping(uint16 => bytes memory) private multihashes;
+    mapping(uint16 => mapping(address => uint64)) signedAt;
 
-    constructor() {
-        _registerInterface(type(IERC5289Library).interfaceId);
-    }
+    constructor() { }
 
     function registerDocument(bytes memory multihash) public {
-        uint48 documentId = counter++;
+        uint16 documentId = counter++;
         multihashes[documentId] = multihash;
-        signatureHashes[documentId] = keccak256(abi.encodePacked(bytes("\x19Sign Legal Document:"), multihash));
     }
 
-    function legalDocument(uint48 documentId) public view returns (bytes memory) {
-        return multihash[documentId];
+    function legalDocument(uint16 documentId) public view returns (bytes memory) {
+        return multihashes[documentId];
     }
 
-    function documentSigned(address user, uint48 documentId) public view returns (boolean signed, uint64 timestamp) {
-        return signed[documentId][user], signedAt[documentId][user];
+    function documentSigned(address user, uint16 documentId) public view returns (boolean isSigned) {
+        return signedAt[documentId][user] != 0;
     }
 
-    function signDocument(address signer, bytes memory signature) public {
-        require(SignatureChecker.isValidSignatureNow(signer, signatureHash[documentId], signature), "Invalid signature");
-        signed[documentId][signer] = true;
-        signedAt[documentId][signer] = uint64(block.timestamp);
-        emit DocumentSigned(signer, documentId);
+    function documentSignedAt(address user, uint16 documentId) public view returns (uint64 timestamp) {
+        return signedAt[documentId][user];
+    }
+
+    function signDocument() public {
+        signedAt[documentId][msg.sender] = uint64(block.timestamp);
+        emit DocumentSigned(msg.sender, documentId);
+    }
+
+    function supportsInterface(bytes4 _interfaceId) public view returns (bool) {
+        return _interfaceId == type(IERC5289Library).interfaceId;
     }
 }
