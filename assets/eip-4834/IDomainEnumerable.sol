bd3b3199518195192d92e24d532993de776cffb1
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2022-08-28 15:11:14 -0400

    Errata: Returning Lists is Bad:tm:

--- assets/eip-4834/IDomainEnumerable.sol
+++ assets/eip-4834/IDomainEnumerable.sol
@@ -7,7 +7,11 @@ import "./IDomain.sol";
 /// @author         Pandapip1
 /// @dev            https://eips.ethereum.org/EIPS/eip-4835
 interface IDomainEnumerable is IDomain {
-    /// @notice     Query all subdomains. Must revert if the list of domains is unknown or infinite.
-    /// @return     The list of all subdomains.
-    function listDomains() external view returns (string[] memory);
+    /// @notice     Query all subdomains. Must revert if the number of domains is unknown or infinite.
+    /// @return     The subdomain with the given index.
+    function subdomainByIndex(uint256 index) external view returns (string memory);
+    
+    /// @notice     Get the total number of subdomains. Must revert if the number of domains is unknown or infinite.
+    /// @return     The total number of subdomains
+    function totalSubdomains() external view returns (uint256);
 }
