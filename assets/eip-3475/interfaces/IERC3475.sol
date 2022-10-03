d9589018d9e0e89f52f6f4da5f6a778598f3dd4b
dhruvmalik007 <malikdhruv1994@gmail.com>
2022-10-03 15:26:48 +0200

    refactoring and addressing ambiguous descriptions

--- assets/eip-3475/interfaces/IERC3475.sol
+++ assets/eip-3475/interfaces/IERC3475.sol
@@ -10,16 +10,16 @@ interface IERC3475 {
     /**
      * @dev Values structure of the Metadata
      */
-    struct Values {        
+    struct Values { 
         string stringValue;
         uint uintValue;
         address addressValue;
         bool boolValue;
     }
     /**
-     * @dev structure allows to define particular bond metadata  of any given number of bonds from one address to another.
+     * @dev structure allows to define particular bond metadata (ie the values in the class as well as nonce inputs). 
      * @title": "defining the title information",
-     * @type": "explaining the  data type of the title information added (eg int, bool, address)",
+     * @type": "explaining the data type of the title information added (eg int, bool, address)",
      * @description": "little description about the information stored in the bond",
      */
     struct Metadata {
@@ -29,7 +29,7 @@ interface IERC3475 {
     }
     /**
      * @dev structure that defines the parameters for specific issuance of bonds and amount which are to be transferred/issued/given allowance, etc.
-     * @notice : this structure is used to streamline the interface parameters vis a vis ERC20 and other token standard.
+     * @notice : this structure is used to streamline the input parameters for functions of this standard with that of other Token standards like ERC20.
      * @classId is the class id of the bond.
      * @nonceId is the nonce id of the given bond class. This param is for distinctions of the issuing conditions of the bond.
      * @amount is the amount of the bond that will be transferred.
@@ -43,7 +43,7 @@ interface IERC3475 {
     // WRITABLES
     /**
      * @dev allows the transfer of a bond from one address to another (either single or in batches).
-     * @param _from  is the address of the holder whose balance is about to decrease.
+     * @param _from is the address of the holder whose balance is about to decrease.
      * @param _to is the address of the recipient whose balance is about to increase.
      * @param _transactions is the object defining {class,nonce and amount of the bonds to be transferred}.
      */
@@ -73,7 +73,7 @@ interface IERC3475 {
     /**
      * @dev allows the transfer of any number of bond types from an address to another.
      * The calling of this function needs to be restricted to bond issuer contract.
-     * @param _from  is the address of the holder whose balance about to decrees.
+     * @param _from is the address of the holder whose balance about to decrees.
      * @param _transactions is the object defining {class,nonce and amount of the bonds to be redeemed for given whitelisted bond}.
      */
     function burn(address _from, Transaction[] calldata _transactions) external;
@@ -138,7 +138,7 @@ interface IERC3475 {
     
     /**
      * @dev Returns the values of the given classId.
-     * @param classId is the specific classId of which we want to  return the parameter.
+     * @param classId is the specific classId of which we want to return the parameter.
      * @param metadataId is the index corresponding to the class parameter that you want to return from mapping.
      * the metadata SHOULD follow a set of structures explained in eip-3475.md
      */
@@ -146,10 +146,10 @@ interface IERC3475 {
    
     /**
      * @dev Returns the values of given nonceId.
-     * @param  metadataId index number of structure as  explained in the metadata section in eip-3475.md
+     * @param metadataId index number of structure as explained in the metadata section in eip-3475.md
      * @param classId is the class of bonds for which you determine the nonce.
      * @param nonceId is the nonce for which you return the value struct info.
-     * Returns the values object corresponding to the given value.   
+     * Returns the values object corresponding to the given value.
      */
     function nonceValues(uint256 classId, uint256 nonceId, uint256 metadataId) external view returns ( Values memory);
     
@@ -172,8 +172,8 @@ interface IERC3475 {
     function allowance(address _owner, address _spender, uint256 classId, uint256 nonceId) external view returns (uint256);
     /**
      * @notice Queries the approval status of an operator for bonds (for all classes and nonce issuances of owner).
-     * @param _owner is the current holder of the bonds for  all classes/nonces.
-     * @param _operator is the address which is  having access to the bonds of _owner for transferring. 
+     * @param _owner is the current holder of the bonds for all classes/nonces.
+     * @param _operator is the address which is having access to the bonds of _owner for transferring. 
      * Returns "true" if the operator is approved, "false" if not.
      */
     function isApprovedFor(address _owner, address _operator) external view returns (bool);
@@ -196,7 +196,7 @@ interface IERC3475 {
      */
     event Burn(address indexed _operator, address indexed _from, Transaction[] _transactions);
     /**
-     * @dev MUST emit when approval for a second party/operator address to manage all bonds from a classId given for an owner address is enabled or disabled (ie putting approval true and false respectively).
+     * @dev MUST emit when approval for a second party/operator address to manage all bonds from a classId given for an owner address is enabled or disabled (absence of an event assumes disabled).
      */
     event ApprovalFor(address indexed _owner, address indexed _operator, bool _approved);
 }
