a65bd288e704805ea3e4b150002195925ae6fb6b
Jeremy Z <111479752+wnft@users.noreply.github.com>
2022-11-16 00:52:30 +0800

    Remove any build system files

--- assets/eip-5496/migrations/1_initial_migration.js
+++
@@ -1,8 +0,0 @@
-const ERC721PDemo = artifacts.require("ERC721PDemo");
-const ERC721PCloneableDemo = artifacts.require("ERC721PCloneableDemo");
-
-module.exports = function (deployer) {
-  deployer.deploy(ERC721PDemo,'ERC721PDemo','EPD');  
-  deployer.deploy(ERC721PCloneableDemo,'ERC721PCDemo','EPCD');  
-};
-
