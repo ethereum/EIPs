b2a8665f2b5230a495e67b49c59c6a9d64a3c4f8
dhruvmalik007 <malikdhruv1994@gmail.com>
2022-12-12 12:01:16 +0100

    update

--- assets/eip-5851/contracts/test.sol
+++ assets/eip-5851/contracts/test.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: CC0-1.0
 pragma solidity ^0.8.0;
-import "./interfaces/IERC.sol";
 import "./verification_modifier.sol";
 
 abstract contract Token is verification_modifier {
--- assets/eip-5851/contracts/verification_modifier.sol
+++ assets/eip-5851/contracts/verification_modifier.sol
@@ -2,11 +2,11 @@
 pragma solidity ^0.8.0;
 import "./interfaces/IERC5851.sol";
 
-abstract contract ERC5851 is IERC5851 {
+abstract contract verification_modifier {
     address private _authenticator;
-    Requirement[] private _KYCRequirement;
+    IERC5851.Requirement[] private _KYCRequirement;
 
-    constructor(address authenticatorAddress, Requirement[] memory KYCStandards) {
+    constructor(address authenticatorAddress, IERC5851.Requirement[] memory KYCStandards) {
         _KYCRequirement = KYCStandards;
         _authenticator = authenticatorAddress;
     }
