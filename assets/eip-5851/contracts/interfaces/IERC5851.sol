c67c2e110be1b782ca80afbbc282632c1478219d
yuliu-debond <79855548+yuliu-debond@users.noreply.github.com>
2022-12-15 11:35:16 +0100

    Update IERC5851.sol

--- assets/eip-5851/contracts/interfaces/IERC5851.sol
+++ assets/eip-5851/contracts/interfaces/IERC5851.sol
@@ -1,94 +1,136 @@
 // SPDX-License-Identifier: CC0-1.0
 pragma solidity ^0.8.0;
 
-interface IERC5851 {
-    // STRUCTURE 
-    /**
-     * @dev metadata and Values structure of the Metadata, cited from ERC-3475.
-     eg: 
-     {
-        "title":"jurisdiction-code",
-        "_type":"string",
-        "description":"ISO code defining the jusrisdiction"
-
-     }
+interface IERC5851{
+    
+    /** Metadata
+    * 
+    * @param title defines the name of the claim field
+    * @param _type is the nature of the data (bool,string,address,bytes,..)
+    * @param description additional information about claim details.
      */
     struct Metadata {
         string title;
         string _type;
         string description;
     }
+    // Values represent the various attributes that users can define in the claims structure on-chain:
 
-    /**    
-    * this stores the actual parameter on which the condition is described.
-    * e.g  
-    * string swissJurisdiction = IERC5851.Values("ch","0",address(0), false)
+    
+    /**
+    Claims structure consist of the conditions and value that holder claims to associate and verifier has to validate them.
+    @notice the below given parameters are for reference purposes only, developers can optimize the fields that are needed to be represented on-chain by using schemes like TLV, encoding into base64 etc.
+    @dev structure that DeFines the parameters for specific claims of the SBT certificate
+    @notice this structure is used for the verification process, it contains the metadata, logic and expectation
+    @logic given here MUST be one of ("⊄", "⊂", "<", "<=", "==", "!=", ">=",">")
     */
+    struct Claims {
+        Metadata metadata;
+        string logic;
+        Values expectation;
+   
+    }
+
+    /**
+     * @dev Values here can be read and wrote by smartcontract and front-end, cited from [EIP-3475](./eip-3475.md) 
+     */   
     struct Values { 
         string stringValue;
         uint uintValue;
         address addressValue;
         bool boolValue;
     }
+
+
+    // getter functions
+
+    /// @notice getter function to validate if the address `verifying` is the holder of the claim Defined by the tokenId `SBTID`
+    /// @dev it MUST be Defining the logic to fetch the result of the ZK verification (either from).
+    /// @dev logic given here MUST be one of ("⊄", "⊂", "<", "<=", "==", "!=", ">=", ">")
+    /// @param verifying is the  EOA address that wants to validate the SBT issued to it by the KYC. 
+    /// @param SBTID is the Id of the SBT that user is the claimer.
+    /// @return true if the assertion is valid, else false
     /**
-     * @dev structure that defines the parameters for specific issuance of bonds and amount which are to be transferred/issued/given allowance, etc.
-     * @notice this structure is used for the verification process, it chontains the metadata, logic and expectation
-     * @logic given here MUST be one of ("⊄", "⊂", "<", "<=", "==", "!=", ">=",">")
-     */
-    struct Requirement {
-        Metadata metadata;
-        string logic;
-        Values expectation;
-    }
-    //getter functions 
-    
-    /**
-    This function gets the status of whether the address `verifying` is holding the address SBTID.
-    @param verifying is the address who wants to verify the holding of KYC-SBT of Id 'SBTID'.
-    @param SBTID is the index number of the SBT issued by the KYC admin.
-    @return returns true if the KYC is verified else false.
+    example ifVerified(0xfoo, 1) => true will mean that 0xfoo is the holder of the SBT identity token DeFined by tokenId of the given collection. 
     */
     function ifVerified(address verifying, uint256 SBTID) external view returns (bool);
 
-    /// @notice getter function to fetch the onchain identification logic for the given identity holder.
+
+    /// @notice getter function to fetch the on-chain identification logic for the given identity holder.
     /// @dev it MUST not be defined for address(0). 
     /// @param SBTID is the Id of the SBT that the user is the claimer.
     /// @return the struct array of all the descriptions of condition metadata that is defined by the administrator for the given KYC provider.
+    /**
+    ex: standardClaim(1) --> {
+    { "title":"age",
+        "type": "uint",
+        "description": "age of the person based on the birth date on the legal document",
+        },
+       "logic": ">=",
+    "value":"18"  
+    }
+    Defines the condition encoded for the identity index 1, DeFining the identity condition that holder must be equal or more than 18 years old.
+    **/
 
-    function standardRequirement(uint256 SBTID) external view returns (Requirement[] memory);
-
-
-    // writable functions 
-    /// @notice function for setting the requirement logic (defined by Requirements metadata) details for the given identity token defined by SBTID.
-    /// @dev it should only be called by the admin address.
-    /// @param SBTID is the Id of the SBT-based identity certificate for which the admin wants to define the Requirements.
-    /// @param `requirements` is the struct array of all the descriptions of condition metadata that is defined by the administrator. check metadata section for more information.
-
-
-    function changeStandardRequirement(uint256 SBTID, Requirement[] calldata requirements) external returns (bool);
+    function standardClaim(uint256 SBTID) external view returns (Claims[] memory);
     
-    
+    // setter functions
+
+        /// @notice function for setting the claim requirement logic (defined by Claims metadata) details for the given identity token defined by SBTID.
+        /// @dev it should only be called by the admin address.
+        /// @param SBTID is the Id of the SBT-based identity certificate for which the admin wants to define the Claims.
+        /// @param `claims` is the struct array of all the descriptions of condition metadata that is defined by the administrator. check metadata section for more information.
+    /**
+    example: changeStandardClaim(1, { "title":"age",
+            "type": "uint",
+            "description": "age of the person based on the birth date on the legal document",
+            },
+        "logic": ">=",
+        "value":"18"  
+    }); 
+    will correspond to the functionality that admin needs to adjust the standard claim for the identification SBT with tokenId = 1, based on the conditions described in the Claims array struct details.
+    **/
+    function changeStandardClaim(uint256 SBTID, Claims[] memory _claims) external returns (bool);
+
     /// @notice function which uses the ZKProof protocol to validate the identity based on the given 
     /// @dev it should only be called by the admin address.
-    /// @param SBTID is the Id of the SBT-based identity certificate for which admin wants to define the Requirements.
+    /// @param SBTID is the Id of the SBT-based identity certificate for which admin wants to define the Claims.
     /// @param certifying is the address that needs to be proven as the owner of the SBT defined by the tokenID.
-
-    
+    /**
+    example: certify(0xA....., 10) means that admin assigns the DID badge with id 10 to the address defined by the `0xA....` wallet.
+    */
     function certify(address certifying, uint256 SBTID) external returns (bool);
 
 
-
     /// @notice function which uses the ZKProof protocol to validate the identity based on the given 
     /// @dev it should only be called by the admin address.
-    /// @param SBTID is the Id of the SBT-based identity certificate for which the admin wants to define the Requirements.
+    /// @param SBTID is the Id of the SBT-based identity certificate for which the admin wants to define the Claims.
     /// @param certifying is the address that needs to be proven as the owner of the SBT defined by the tokenID.
-
+    // eg: revoke(0xfoo,1): means that KYC admin revokes the SBT certificate number 1 for the address '0xfoo'.
     function revoke(address certifying, uint256 SBTID) external returns (bool);
 
 
-    //EVENTS
-
-    event standardChanged(uint256 SBTID, Requirement[]);   
-    event certified(address certifying, uint256 SBTID);
-    event revoked(address certifying, uint256 SBTID);
+// Events
+    /** 
+    * standardChanged
+    * @notice standardChanged MUST be triggered when claims are changed by the admin. 
+    * @dev standardChanged MUST also be triggered for the creation of a new SBTID.
+    e.g : emit StandardChanged(1, Claims(Metadata('age', 'uint', 'age of the person based on the birth date on the legal document' ), ">=", "18");
+    is emitted when the Claim condition is changed which allows the certificate holder to call the functions with the modifier, claims that the holder must be equal or more than 18 years old.
+    */
+    event StandardChanged(uint256 SBTID, Claims[] _claims);
+    
+    /** 
+    * certified
+    * @notice certified MUST be triggered when the SBT certificate is given to the certifying address. 
+    * eg: Certified(0xfoo,2); means that wallet holder address 0xfoo is certified to hold a certificate issued with id 2, and thus can satisfy all the conditions defined by the required interface.
+    */
+    event Certified(address certifying, uint256 SBTID);
+    
+    /** 
+    * revoked
+    * @notice revoked MUST be triggered when the SBT certificate is revoked. 
+    * eg: Revoked( 0xfoo,1); means that entity user 0xfoo has been revoked to all the function access defined by the SBT ID 1.
+    */
+    event Revoked(address certifying, uint256 SBTID);
 }
