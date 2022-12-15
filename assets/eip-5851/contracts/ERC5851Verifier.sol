44738bd343d1bd7cf72bf851a4f77301c7d9101d
yuliu-debond <79855548+yuliu-debond@users.noreply.github.com>
2022-12-15 14:07:07 +0100

    Create ERC5851Verifier.sol

--- assets/eip-5851/contracts/ERC5851Verifier.sol
+++ assets/eip-5851/contracts/ERC5851Verifier.sol
@@ -3,16 +3,15 @@ pragma solidity ^0.8.0;
 import "./interfaces/IERC5851.sol";
 
 abstract contract ERC5851Verifier is IERC5851 {
-    address private _authenticator;
-    Verification[] private _KYCClaim;
+    address private _issuer;
 
-    constructor(address issuer, Verification[] memory KYCStandards) {
-        _KYCClaim = KYCStandards;
+    constructor(address issuer) {
         _issuer = issuer;
     }
 
-    modifier KYCApproved(address claimer) {
-        IERC5851(_issuer).ifVerified(claimer, uint256 SBTID);
+    modifier KYCApproved(address claimer, uint256 SBTID) {
+        IERC5851(_issuer).ifVerified(claimer, SBTID);
         _;
     }
+
 }
