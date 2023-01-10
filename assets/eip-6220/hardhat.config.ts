4ba96f4d0cadf62b54f1dc0b1e95eb688dc7781b
steven2308 <steven2308@outlook.com>
2023-01-10 16:15:08 -0500

    EIP-6220: Leaves only minimal needed packages to compile and run tests.

--- assets/eip-6220/hardhat.config.ts
+++ assets/eip-6220/hardhat.config.ts
@@ -2,9 +2,6 @@ import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-chai-matchers';
 import '@nomiclabs/hardhat-etherscan';
 import '@typechain/hardhat';
-import 'hardhat-contract-sizer';
-import 'hardhat-gas-reporter';
-import 'solidity-coverage';
 
 const config: HardhatUserConfig = {
   solidity: {
