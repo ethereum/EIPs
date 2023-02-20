b03fc034263819be01c518419e21f4d217ba2f6a
Pandapip1 <gavinnjohn@gmail.com>
2023-02-20 12:31:32 -0500

    Really *really* hacky search bar

--- .vitepress/theme/components/GoogleSearch.vue
+++ .vitepress/theme/components/GoogleSearch.vue
@@ -9,25 +9,23 @@ let searchitem = ref("");
         <div class="gcse-search"></div>
     </div>
     <div id="docsearch" style="margin-right: 1em;">
-        <button type="button" class="DocSearch DocSearch-Button" aria-label="Search">
+        <form type="button" class="DocSearch DocSearch-Button" aria-label="Search">
             <span class="DocSearch-Button-Container">
                 <svg class="DocSearch-Search-Icon" width="20" height="20" viewBox="0 0 20 20">
                     <path d="M14.386 14.386l4.0877 4.0877-4.0877-4.0877c-2.9418 2.9419-7.7115 2.9419-10.6533 0-2.9419-2.9418-2.9419-7.7115 0-10.6533 2.9418-2.9419 7.7115-2.9419 10.6533 0 2.9419 2.9418 2.9419 7.7115 0 10.6533z" stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round" style="--darkreader-inline-stroke: currentColor;" data-darkreader-inline-stroke=""></path>
                 </svg>
-                <input class="DocSearch-Button-Placeholder" id="searchitem" v-model="searchitem"/>
+                <input class="DocSearch-Button-Placeholder" id="searchitem" v-model="searchitem" placeholder="Enter search here..."/>
                 <a class="DocSearch-Button-Keys" :href="`#gsc.tab=0&gsc.sort=&gsc.q=${searchitem}`" id="clicksearch">
                     <kbd class="DocSearch-Button-Key"></kbd>
                     <kbd class="DocSearch-Button-Key">Search</kbd>
                 </a>
+                <input type="submit" class="DocSearch-Button-Placeholder" value="Search" style="display: none;" id="searchsubmit"/>
             </span>
-        </button>
+        </form>
     </div>
     <component :is="'script'">
-        document.getElementById("searchitem").addEventListener("keyup", function(event) {
-            if (event.keyCode === 13) {
-                event.preventDefault();
-                document.getElementById("clicksearch").click();
-            }
+        document.getElementById("searchsubmit").addEventListener("click", function() {
+            document.getElementById("clicksearch").click();
         });
     </component>
 </template>
