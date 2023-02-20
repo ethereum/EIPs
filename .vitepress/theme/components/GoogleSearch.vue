1e83b739b3223ee83b3d45f6757da60aff24108c
Pandapip1 <gavinnjohn@gmail.com>
2023-02-20 12:16:21 -0500

    Hacky custom search bar

--- .vitepress/theme/components/GoogleSearch.vue
+++ .vitepress/theme/components/GoogleSearch.vue
@@ -4,6 +4,28 @@
     <div>
         <div class="gcse-search"></div>
     </div>
+    <div id="docsearch" style="margin-right: 1em;">
+        <button type="button" class="DocSearch DocSearch-Button" aria-label="Search">
+            <span class="DocSearch-Button-Container">
+                <svg class="DocSearch-Search-Icon" width="20" height="20" viewBox="0 0 20 20">
+                    <path d="M14.386 14.386l4.0877 4.0877-4.0877-4.0877c-2.9418 2.9419-7.7115 2.9419-10.6533 0-2.9419-2.9418-2.9419-7.7115 0-10.6533 2.9418-2.9419 7.7115-2.9419 10.6533 0 2.9419 2.9418 2.9419 7.7115 0 10.6533z" stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round" style="--darkreader-inline-stroke: currentColor;" data-darkreader-inline-stroke=""></path>
+                </svg>
+                <input class="DocSearch-Button-Placeholder" id="searchitem"/>
+                <a class="DocSearch-Button-Keys" :href="`#gsc.tab=0&gsc.sort=&gsc.q=test`" id="clicksearch">
+                    <kbd class="DocSearch-Button-Key"></kbd>
+                    <kbd class="DocSearch-Button-Key">Search</kbd>
+                </a>
+            </span>
+        </button>
+    </div>
+    <component :is="'script'">
+        document.getElementById("searchitem").addEventListener("keyup", function(event) {
+            if (event.keyCode === 13) {
+                event.preventDefault();
+                document.getElementById("clicksearch").click();
+            }
+        });
+    </component>
 </template>
 <style>
 /* Overrides the default Google search box styles */
@@ -11,6 +33,9 @@
     background-color: inherit !important;
     border: none !important;
     color: #000000 !important;
+}
+.gsc-search-box-tools { /* Make this disappear; We're using our own */
+    display: none !important;
 }
 .gsc-completion-container {
     background-color: #ffffff !important;
