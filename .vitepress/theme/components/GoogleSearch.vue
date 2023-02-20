defa1f2495d380175d920b14b2fdd8049a79f5fa
Pandapip1 <gavinnjohn@gmail.com>
2023-02-20 12:18:51 -0500

    Fix incorrect search terming

--- .vitepress/theme/components/GoogleSearch.vue
+++ .vitepress/theme/components/GoogleSearch.vue
@@ -1,3 +1,7 @@
+<script setup>
+import { ref } from "vue";
+let searchitem = ref("");
+</script>
 <template>
     <!-- Vue doesn't like the built in script thing. Use the component thing to get around it. -->
     <component :is="'script'" async src="https://cse.google.com/cse.js?cx=d55df9aacc78f45ed"></component>
@@ -10,8 +14,8 @@
                 <svg class="DocSearch-Search-Icon" width="20" height="20" viewBox="0 0 20 20">
                     <path d="M14.386 14.386l4.0877 4.0877-4.0877-4.0877c-2.9418 2.9419-7.7115 2.9419-10.6533 0-2.9419-2.9418-2.9419-7.7115 0-10.6533 2.9418-2.9419 7.7115-2.9419 10.6533 0 2.9419 2.9418 2.9419 7.7115 0 10.6533z" stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round" style="--darkreader-inline-stroke: currentColor;" data-darkreader-inline-stroke=""></path>
                 </svg>
-                <input class="DocSearch-Button-Placeholder" id="searchitem"/>
-                <a class="DocSearch-Button-Keys" :href="`#gsc.tab=0&gsc.sort=&gsc.q=test`" id="clicksearch">
+                <input class="DocSearch-Button-Placeholder" id="searchitem" v-model="searchitem"/>
+                <a class="DocSearch-Button-Keys" :href="`#gsc.tab=0&gsc.sort=&gsc.q=${searchitem}`" id="clicksearch">
                     <kbd class="DocSearch-Button-Key"></kbd>
                     <kbd class="DocSearch-Button-Key">Search</kbd>
                 </a>
