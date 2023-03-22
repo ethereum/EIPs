237b019aba6f34993b4ccf36023ba90b861cab8f
ArchieR7 <Archie@Archie.tw>
2023-03-22 19:16:35 +0800

    feat(erc6672): modify redemptions with EnumerableSet

--- assets/eip-6672/contracts/ERC6672.sol
+++ assets/eip-6672/contracts/ERC6672.sol
@@ -2,29 +2,45 @@
 pragma solidity 0.8.16;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "./interfaces/IERC6672.sol";
 
 contract ERC6672 is ERC721, IERC6672 {
+    using EnumerableSet for EnumerableSet.Bytes32Set;
+    uint256 public nowTokenId;
+
+    bytes4 public constant IERC6672_ID = type(IERC6672).interfaceId;
+    bytes4 public constant IERC721_ID = type(IERC721).interfaceId;
     mapping(address => mapping(uint256 => mapping(bytes32 => bool))) redemptionStatus;
     mapping(address => mapping(uint256 => mapping(bytes32 => string))) public memos;
-    mapping(address => mapping(uint256 => bytes32[])) redemptions;
+    mapping(address => mapping(uint256 => EnumerableSet.Bytes32Set)) redemptions;
 
-    constructor() ERC721("MultiRedeemableNFT", "mrNFT") {}
+    constructor() ERC721("Multiple RedeemableNFT", "mrNFT") {}
+
+    function airdropMint(uint numberOfTokens, address to) external {
+        uint256 _nowTokenId = nowTokenId;
+
+        for(uint i = 1; i <= numberOfTokens; i++) {
+            _safeMint(to, _nowTokenId + i);
+        }
+
+        nowTokenId += numberOfTokens;
+    }
 
     function isRedeemed(address _operator, bytes32 _redemptionId, uint256 _tokenId) external view returns (bool) {
         return _isRedeemed(_operator, _redemptionId, _tokenId);
     }
 
     function getRedemptionIds(address _operator, uint256 _tokenId) external view returns (bytes32[] memory) {
-        require(redemptions[_operator][_tokenId].length > 0, "ERC6672: token doesn't have any redemptions.");
-        return redemptions[_operator][_tokenId];
+        require(redemptions[_operator][_tokenId].length() > 0, "ERC6672: token doesn't have any redemptions.");
+        return redemptions[_operator][_tokenId].values();
     }
     
     function redeem(bytes32 _redemptionId, uint256 _tokenId, string memory _memo) external {
         address _operator = msg.sender;
         require(!_isRedeemed(_operator, _redemptionId, _tokenId), "ERC6672: token already redeemed.");
         _update(_operator, _redemptionId, _tokenId, _memo, true);
-        redemptions[_operator][_tokenId].push(_redemptionId);
+        redemptions[_operator][_tokenId].add(_redemptionId);
     }
 
     function cancel(bytes32 _redemptionId, uint256 _tokenId, string memory _memo) external {
@@ -45,19 +61,6 @@ contract ERC6672 is ERC721, IERC6672 {
     }
 
     function _removeRedemption(address _operator, bytes32 _redemptionId, uint256 _tokenId) internal {
-        bytes32[] storage _redemptions = redemptions[_operator][_tokenId];
-        for (uint i = 0; i < _redemptions.length; i++) {
-            if (_redemptions[i] == _redemptionId) {
-                if (i == _redemptions.length - 1) {
-                    _redemptions.pop();
-                } else {
-                    for (uint j = 0; j < _redemptions.length - 1; j++) {
-                        _redemptions[j] = _redemptions[j+1];
-                    }
-                }
-                redemptions[_operator][_tokenId] = _redemptions;
-                return;
-            }
-        }
+        redemptions[_operator][_tokenId].remove(_redemptionId);
     }
 }
