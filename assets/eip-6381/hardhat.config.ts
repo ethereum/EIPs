52d78c5d76f2627f259bc2c8f0830c2764960c95
Jan Turk <jan.turk12@gmail.com>
2023-04-04 10:50:46 +0200

    Update EIP-6381: Update EIP-6381 to be a public good repository
    Merged by EIP-Bot.

--- assets/eip-6381/hardhat.config.ts
+++ assets/eip-6381/hardhat.config.ts
@@ -1,10 +1,10 @@
-import { HardhatUserConfig } from 'hardhat/config';
-import '@nomicfoundation/hardhat-chai-matchers';
-import '@typechain/hardhat';
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-chai-matchers";
+import "@typechain/hardhat";
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: '0.8.16',
+    version: "0.8.16",
     settings: {
       optimizer: {
         enabled: true,
