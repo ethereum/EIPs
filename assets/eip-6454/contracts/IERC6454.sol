23c66d1e665583983d643fad7fe95268b6ef8ecf
Jan Turk <jan.turk12@gmail.com>
2023-05-09 16:15:02 +0200

    Update EIP-6454 remove the simple method
    After further discussion it became clear, that the proposal doesn't need
    two methods, since you can get the behaviour of the simple method using
    the more expansive one. The specification and assets were updated to
    reflect this.

--- assets/eip-6454/contracts/IERC6454.sol
+++ assets/eip-6454/contracts/IERC6454.sol
@@ -3,20 +3,11 @@
 pragma solidity ^0.8.16;
 
 interface IERC6454 {
-
     /**
      * @notice Used to check whether the given token is transferable or not.
-     * @dev If this function returns `false`, the transfer of the token MUST revert execution
-     * @dev If the tokenId does not exist, this method MUST revert execution
-     * @param tokenId ID of the token being checked
-     * @return Boolean value indicating whether the given token is transferable
-     */
-    function isTransferable(uint256 tokenId) external view returns (bool);
-
-    /**
-     * @notice Used to check whether the given token is transferable or not based on source and destination address.
-     * @dev If this function returns `false`, the transfer of the token MUST revert execution
-     * @dev If the tokenId does not exist, this method MUST revert execution
+     * @dev If this function returns `false`, the transfer of the token MUST revert execution.
+     * @dev If the tokenId does not exist, this method MUST revert execution, unless the token is being checked for
+     *  minting.
      * @param tokenId ID of the token being checked
      * @param from Address from which the token is being transferred
      * @param to Address to which the token is being transferred
