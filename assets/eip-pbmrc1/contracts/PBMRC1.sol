0bc5a9ab129dd346b166ba9892a62fb25d68ab88
Wong Tse Jian <wongtsejian@gmail.com>
2023-05-18 13:30:38 +0800

    edits to param names,corrected errata in I5679

--- assets/eip-pbmrc1/contracts/PBMRC1.sol
+++ assets/eip-pbmrc1/contracts/PBMRC1.sol
@@ -33,8 +33,8 @@ interface IERC173 {
 /// The EIP-165 identifier of this interface is 0xf4cedd5a
 interface IERC5679Ext1155 {
    function safeMint(address _to, uint256 _id, uint256 _amount, bytes calldata _data) external;
-   function safeMintBatch(address to, uint256[] calldata ids, uint256[] calldata amounts, bytes calldata data) external;
-   function burn(address _from, uint256 _id, uint256 _amount, bytes[] calldata _data) external;
+   function safeMintBatch(address _to, uint256[] calldata ids, uint256[] calldata amounts, bytes calldata _data) external;
+   function burn(address _from, uint256 _id, uint256 _amount, bytes calldata _data) external;
    function burnBatch(address _from, uint256[] calldata ids, uint256[] calldata amounts, bytes calldata _data) external;
 }
 
