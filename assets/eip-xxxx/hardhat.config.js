02fc214b8448636d53762e5d88f1f4ce1842efb8
Francesco Sullo <sullof@gmail.com>
2023-05-30 16:18:17 -0700

    wip

---
+++ assets/eip-xxxx/hardhat.config.js
@@ -0,0 +1,28 @@
+require("@nomiclabs/hardhat-waffle");
+require("@openzeppelin/hardhat-upgrades");
+require("hardhat-contract-sizer");
+require("solidity-coverage");
+
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      blockGasLimit: 10000000,
+    }
+  },
+};
