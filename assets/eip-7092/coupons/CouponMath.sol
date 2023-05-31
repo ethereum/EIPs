f19c2d0600663a07265f65188615eba028e24bbc
sam say <80607135+Edoumou@users.noreply.github.com>
2023-05-31 11:45:53 +0200

    Create CouponMath.sol

---
+++ assets/eip-7092/coupons/CouponMath.sol
@@ -0,0 +1,54 @@
+pragma solidity ^0.8.0;
+
+import "../IERC7092.sol";
+
+library CouponMath {
+    /**
+    * @notice Returns the annual interest earned by an account in bips
+    * @param _investor the investor account address
+    * @param _bondContract the bond contract address
+    */
+    function annualInterest(
+        address _investor,
+        address _bondContract
+    ) external view returns(uint256) {
+        uint256 couponRate = IERC(_bondContract).couponRate();
+        uint256 denomination = IERC(_bondContract).denomination();
+        uint256 principal = IERC(_bondContract).principalOf(_investor);
+
+        return principal * denomination * couponRate;
+    }
+
+    /**
+    * @notice Returns the interest earned by an account in period `_duration` in bips
+    * @param _investor the investor account address
+    * @param _duration time ellapsed since the last coupon payment
+    * @param _bondContract the bond contract address
+    */
+    function interest(
+        address _investor,
+        uint256 _duration,
+        address _bondContract
+    ) external view returns(uint256) {
+        uint256 couponRate = IERC(_bondContract).couponRate();
+        uint256 couponDenomination = IERC(_bondContract).denomination();
+        uint256 principal = IERC(_bondContract).principalOf(_investor);
+        uint256 frequency = IERC(_bondContract).couponFrequency();
+        uint256 numberOfDays = _numberOfDays(_bondContract);
+
+        return principal * couponDenomination * couponRate * _duration / (frequency * numberOfDays);
+    }
+
+
+    function _numberOfDays(address _bondContract) private view returns(uint256) {
+        uint256 dayCountBasis = IERC(_bondContract).dayCountBasis();
+
+        if(dayCountBasis == 0) {
+            return 365;
+        } else if(dayCountBasis == 1) {
+            return 360;
+        } else {
+            revert("invalid day count basis value");
+        }
+    }
+}
