b5eb2edd4e9528fb2cb530ea7a848df8d4bafde6
Francesco Sullo <sullof@gmail.com>
2023-06-11 11:29:15 -0700

    Minor change to wording

--- assets/eip-7108/contracts/IERC7108Enumerable.sol
+++ assets/eip-7108/contracts/IERC7108Enumerable.sol
@@ -13,9 +13,8 @@ interface IERC7108Enumerable {
    * @notice Retrieves the balance of tokens a wallet owns within a specific cluster
    * @dev The balance is the number of tokens owned by the caller within the specified cluster.
       Note that due to potential computational complexity, this function could be gas-intensive,
-      and therefore should primarily be called from dApps rather than included in smart contract
-      business logic. To avoid risks, this function should be implemented as 'external' to be
-      sure the smart contract cannot call it internally.
+      and therefore should only be called from dApps rather than internally
+      or from other smart contracts.
    * @param owner The owner of the tokens
    * @param clusterId ID of the cluster
    * @return uint256 Balance of tokens within the cluster
