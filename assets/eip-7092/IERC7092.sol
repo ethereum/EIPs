1d767786b8991a672ebecc4c8f31e2cd805b8706
sam say <80607135+Edoumou@users.noreply.github.com>
2023-10-04 14:21:42 +0200

    update EIP-7092 with cross-chain functionalities

--- assets/eip-7092/IERC7092.sol
+++ assets/eip-7092/IERC7092.sol
@@ -17,13 +17,17 @@ interface IERC7092 {
 
     /**
     * @notice Returns the bond symbol
+    *         It is RECOMMENDED to represent the symbol as a combination of the issuer Issuer'shorter name and the maturity date
+    *         Ex: If a company named Green Energy issues bonds that will mature on october 25, 2030, the bond symbol could be `GE30` or `GE2030` or `GE102530`
+    *
+    * OPTIONAL - interfaces and other contracts MUST NOT expect these values to be present. The method is used to improve usability.
     */
     function symbol() external view returns(string memory);
 
     /**
     * @notice Returns the number of decimals the bond uses - e.g `10`, means to divide the token amount by `10000000000`
     *
-    * OPTIONAL
+    * OPTIONAL - interfaces and other contracts MUST NOT expect these values to be present. The method is used to improve usability.
     */
     function decimals() external view returns(uint8);
 
@@ -34,6 +38,8 @@ interface IERC7092 {
 
     /**
     * @notice Returns the copoun currency. This is the contract address of the token used to pay coupons. It can be same as the the one used for the principal
+    *
+    * OPTIONAL - interfaces and other contracts MUST NOT expect these values to be present. The method is used to improve usability.
     */
     function currencyOfCoupon() external view returns(address);
 
@@ -45,10 +51,16 @@ interface IERC7092 {
 
     /**
     * @notice Returns the issue volume (total debt amount). It is RECOMMENDED to express the issue volume in denomination unit.
-    *         ex: if denomination = $1,000, and the total debt is $5,000,000
-    *         then, issueVolume() = $5,000, 000 / $1,000 = 5,000 bonds
     */
     function issueVolume() external view returns(uint256);
+
+    /**
+    * @notice Returns the total token supply
+    *
+    * OPTIONAL - interfaces and other contracts MUST NOT expect these values to be present. The method is used in case a bond needs to be represented as en ERC20 token.
+    *            If not implemented, the total supply is equal to the ratio => issueVolume / denomination.
+    */
+    function totalSupply() external view returns(uint256);
 
     /**
     * @notice Returns the bond interest rate. It is RECOMMENDED to express the interest rate in basis point unit.
@@ -59,12 +71,16 @@ interface IERC7092 {
 
     /**
     * @notice Returns the coupon type
-    *         ex: 0: Zero coupon, 1: Fixed Rate, 2: Floating Rate, etc...
+    *         An example could be => 0: Zero coupon, 1: Fixed Rate, 2: Floating Rate, etc...
+    *
+    * OPTIONAL - interfaces and other contracts MUST NOT expect these values to be present. The method is used to improve usability.
     */
     function couponType() external view returns(uint256);
 
     /**
     * @notice Returns the coupon frequency, i.e. the number of times coupons are paid in a year.
+    *
+    * * OPTIONAL - interfaces and other contracts MUST NOT expect these values to be present. The method is used to improve usability.
     */
     function couponFrequency() external view returns(uint256);
 
@@ -81,17 +97,25 @@ interface IERC7092 {
 
     /**
     * @notice Returns the day count basis
-    *         Ex: 0: actual/actual, 1: actual/360, etc...
+    *         An example could be => 0: actual/actual, 1: actual/360, etc...
+    *
+    * OPTIONAL - interfaces and other contracts MUST NOT expect these values to be present. The method is used to improve usability.
     */
     function dayCountBasis() external view returns(uint256);
 
     /**
-    * @notice Returns the principal of an account. It is RECOMMENDED to express the principal in denomination unit.
-    *         Ex: if denomination = $1,000, and the user has invested $5,000
-    *             then principalOf(_account) = 5,000/1,000 = 5
+    * @notice Returns the principal of an account. It is RECOMMENDED to express the principal in the bond currency unit (USDC, DAI, etc...)
     * @param _account account address
     */
     function principalOf(address _account) external view returns(uint256);
+
+    /**
+    * @notice returns the balance of `_account`
+    *
+    * OPTIONAL - interfaces and other contracts MUST NOT expect these values to be present. The method is used in case a bond needs to be represented as en ERC20 token.
+    *            If not implemented, the balance of an account is equal to the ratio => principalOf(account) / denomination.
+    */
+    function balanceOf(address _account) external view returns(uint256);
 
     /**
     * @notice Returns the amount of tokens the `_spender` account has been authorized by the `_owner``
@@ -99,88 +123,198 @@ interface IERC7092 {
     * @param _owner the bondholder address
     * @param _spender the address that has been authorized by the bondholder
     */
-    function approval(address _owner, address _spender) external view returns(uint256);
+    function allowance(address _owner, address _spender) external view returns(uint256);
 
     /**
-    * @notice Authorizes `_spender` account to manage `_amount`of their bonds
+    * @notice Authorizes `_spender` account to manage `_amount`of their bond tokens
     * @param _spender the address to be authorized by the bondholder
-    * @param _amount amount of bond to approve. _amount MUST be a multiple of denomination
+    * @param _amount amount of bond tokens to approve
     */
-    function approve(address _spender, uint256 _amount) external;
+    function approve(address _spender, uint256 _amount) external returns(bool);
 
     /**
-    * @notice Authorizes the `_spender` account to manage all their bonds
+    * @notice Authorizes `_spender` account to manage `_amount`of their bond tokens in the destination Chain
     * @param _spender the address to be authorized by the bondholder
+    * @param _amount amount of bond tokens to approve
+    * @param _destinationChainID The unique ID that identifies the destination Chain.
+    * @param _destinationContract The smart contract to interact with in the destination Chain in order to Deposit or Mint tokens that are transferred.
+    *
+    * OPTIONAL - interfaces and other contracts MUST NOT expect this function to be present. The method is used to approve tokens cross-chain.
     */
-    function approveAll(address _spender) external;
+    function crossChainApprove(address _spender, uint256 _amount, uint64 _destinationChainID, address _destinationContract) external returns(bool);
 
     /**
     * @notice Lowers the allowance of `_spender` by `_amount`
     * @param _spender the address to be authorized by the bondholder
-    * @param _amount amount of bond to remove approval; _amount MUST be a multiple of denomination
+    * @param _amount amount of bond tokens to remove from allowance
     */
     function decreaseAllowance(address _spender, uint256 _amount) external;
 
     /**
-    * @notice Removes the allowance for `_spender`
-    * @param _spender the address to remove the authorization by from
+    * @notice Lowers the allowance of `_spender` by `_amount` in the destination Chain
+    * @param _spender the address to be authorized by the bondholder
+    * @param _amount amount of bond tokens to remove from allowance
+    * @param _destinationChainID The unique ID that identifies the destination Chain.
+    * @param _destinationContract The smart contract to interact with in the destination Chain in order to Deposit or Mint tokens that are transferred.
+    *
+    * OPTIONAL - interfaces and other contracts MUST NOT expect this function to be present. The method is used to decrease the token allowance cross-chain.
     */
-    function decreaseAllowanceForAll(address _spender) external;
+    function crossChainDecreaseAllowance(address _spender, uint256 _amount, uint64 _destinationChainID, address _destinationContract) external;
 
     /**
     * @notice Moves `_amount` bonds to address `_to`
     * @param _to the address to send the bonds to
-    * @param _amount amount of bond to transfer. _amount MUST be a multiple of denomination
-    * @param _data additional information provided by the token holder
+    * @param _amount amount of bond tokens to transfer
     */
-    function transfer(address _to, uint256 _amount, bytes calldata _data) external;
+    function transfer(address _to, uint256 _amount) external returns(bool);
 
     /**
-    * @notice Moves all bonds to address `_to`
+    * @notice Moves `_amount` bonds to address `_to`. This methods also allows to attach data to the token that is being transferred
     * @param _to the address to send the bonds to
+    * @param _amount amount of bond tokens to transfer
     * @param _data additional information provided by the token holder
     */
-    function transferAll(address _to, bytes calldata _data) external;
+    function transferWithData(address _to, uint256 _amount, bytes calldata _data) external returns(bool);
+
+    /**
+    * @notice Moves `_amount` bond tokens to address `_to` from the current Chain to another Chain (Ex: move tokens from Ethereum to Polygon)
+    * @param _to the address to send the bonds to
+    * @param _amount amount of bond tokens to transfer
+    * @param _destinationChainID The unique ID that identifies the destination Chain.
+    * @param _destinationContract The smart contract to interact with in the destination Chain in order to Deposit or Mint tokens that are transferred.
+    *
+    * OPTIONAL - interfaces and other contracts MUST NOT expect this function to be present. The method is used to transfer tokens cross-chain.
+    */
+    function crossChainTransfer(address _to, uint256 _amount, uint64 _destinationChainID, address _destinationContract) external returns(bool);
+   
+    /**
+    * @notice Moves `_amount` bonds to address `_to` from the current Chain to another Chain (Ex: move tokens from Ethereum to Polygon).
+    *         This methods also allows to attach data to the token that is being transferred
+    * @param _to the address to send the bonds to
+    * @param _amount amount of bond tokens to transfer
+    * @param _data additional information provided by the token holder
+    * @param _destinationChainID The unique ID that identifies the destination Chain.
+    * @param _destinationContract The smart contract to interact with in the destination Chain in order to Deposit or Mint tokens that are transferred.
+    *
+    * OPTIONAL - interfaces and other contracts MUST NOT expect this function to be present. The method is used to transfer tokens cross-chain.
+    */
+    function crossChainTransferWithData(address _to, uint256 _amount, bytes calldata _data, uint64 _destinationChainID, address _destinationContract) external returns(bool);
 
     /**
     * @notice Moves `_amount` bonds from an account that has authorized the caller through the approve function
     * @param _from the bondholder address
     * @param _to the address to transfer bonds to
-    * @param _amount amount of bond to transfer. _amount MUST be a multiple of denomination
-    * @param _data additional information provided by the token holder
+    * @param _amount amount of bond tokens to transfer
     */
-    function transferFrom(address _from, address _to, uint256 _amount, bytes calldata _data) external;
+    function transferFrom(address _from, address _to, uint256 _amount) external returns(bool);
 
     /**
-    * @notice Moves all bonds from `_from` to `_to`. The caller must have been authorized through the approve function
+    * @notice Moves `_amount` bonds from an account that has authorized the caller through the approve function
+    *         This methods also allows to attach data to the token that is being transferred
     * @param _from the bondholder address
     * @param _to the address to transfer bonds to
+    * @param _amount amount of bond tokens to transfer.
     * @param _data additional information provided by the token holder
     */
-    function transferAllFrom(address _from, address _to, bytes calldata _data) external;
+    function transferFromWithData(address _from, address _to, uint256 _amount, bytes calldata _data) external returns(bool);
 
     /**
-    * @notice MUST be emitted when bonds are transferred
+    * @notice Moves `_amount` bonds from an account that has authorized the caller through the approve function, from the current Chain to another Chain (Ex: move tokens from Ethereum to Polygon)
+    * @param _from the bondholder address
+    * @param _to the address to transfer bonds to
+    * @param _amount amount of bond tokens to transfer
+    * @param _data additional information provided by the token holder
+    * @param _destinationChainID The unique ID that identifies the destination Chain
+    *
+    ** OPTIONAL - interfaces and other contracts MUST NOT expect this function to be present. The method is used to transfer tokens cross-chain.
+    */
+    function crossChainTransferFrom(address _from, address _to, uint256 _amount, uint64 _destinationChainID, address _destinationContract) external returns(bool);
+    
+    /**
+    * @notice Moves `_amount` bonds from an account that has authorized the caller through the approve function, from the current Chain to another Chain (Ex: move tokens from Ethereum to Polygon)
+    *         This methods also allows to attach data to the token that is being transferred
+    * @param _from the bondholder address
+    * @param _to the address to transfer bonds to
+    * @param _amount amount of bond tokens to transfer
+    * @param _data additional information provided by the token holder
+    * @param _destinationChainID The unique ID that identifies the destination Chain.
+    * @param _destinationContract The smart contract to interact with in the destination Chain in order to Deposit or Mint tokens that are transferred.
+    *
+    ** OPTIONAL - interfaces and other contracts MUST NOT expect this function to be present. The method is used to transfer tokens cross-chain.
+    */
+    function crossChainTransferFromWithData(address _from, address _to, uint256 _amount, bytes calldata _data, uint64 _destinationChainID, address _destinationContract) external returns(bool);
+
+    /**
+    * @notice MUST be emitted when bond tokens are transferred
     * @param _from the account that owns bonds
     * @param _to the account that receives the bond
-    * @param _amount the amount of bonds to be transferred
+    * @param _amount amount of bond tokens to be transferred
+    */
+    event Transfer(address _from, address _to, uint256 _amount);
+
+    /**
+    * @notice MUST be emitted when bond tokens are transferred with additional data
+    * @param _from the account that owns bonds
+    * @param _to the account that receives the bond
+    * @param _amount amount of bond tokens to be transferred
     * @param _data additional information provided by the token holder
     */
-    event Transferred(address _from, address _to, uint256 _amount, bytes _data);
+    event TransferWithData(address _from, address _to, uint256 _amount, bytes _data);
+
+    /**
+    * @notice MUST be emitted when bond tokens are transferred cross-chain
+    * @param _from the account that owns bonds
+    * @param _to the account that receives the bond
+    * @param _amount amount of bond tokens to be transferred
+    * @param _destinationChainID The unique ID that identifies the destination Chain.
+    * @param _destinationContract The smart contract to interact with in the destination Chain in order to Deposit or Mint tokens that are transferred.
+    */
+    event CrossChainTransfer(address _from, address _to, uint256 _amount, uint64 _destinationChainID, address _destinationContract);
+
+    /**
+    * @notice MUST be emitted when bond tokens are transferred cross-chain with additional data
+    * @param _from the account that owns bonds
+    * @param _to the account that receives the bond
+    * @param _amount amount of bond tokens to be transferred
+    * @param _data additional information provided by the token holder
+    * @param _destinationChainID The unique ID that identifies the destination Chain.
+    * @param _destinationContract The smart contract to interact with in the destination Chain in order to Deposit or Mint tokens that are transferred.
+    */
+    event CrossChainTransferWithData(address _from, address _to, uint256 _amount, bytes calldata _data, uint64 _destinationChainID, address _destinationContract);
 
     /**
     * @notice MUST be emitted when an account is approved
     * @param _owner the bonds owner
     * @param _spender the account to be allowed to spend bonds
-    * @param _amount the amount allowed by _owner to be spent by _spender.
+    * @param _amount amount of bond tokens allowed by _owner to be spent by _spender.
     */
-    event Approved(address _owner, address _spender, uint256 _amount);
+    event Approval(address _owner, address _spender, uint256 _amount);
+
+    /**
+    * @notice MUST be emitted when an account is approved cross-chain
+    * @param _owner the bonds owner
+    * @param _spender the account to be allowed to spend bonds
+    * @param _amount amount of bond tokens allowed by _owner to be spent by _spender.
+    * @param _destinationChainID The unique ID that identifies the destination Chain.
+    * @param _destinationContract The smart contract to interact with in the destination Chain in order to Deposit or Mint tokens that are transferred.
+    */
+    event CrossChainApproval(address _owner, address _spender, uint256 _amount);
 
     /**
     * @notice MUST be emmitted when the `_owner` decreases allowance from `_sepnder` by quantity `_amount`
     * @param _owner the bonds owner
     * @param _spender the account that has been allowed to spend bonds
-    * @param _amount the amount of tokens to disapprove
+    * @param _amount amount of bond tokens to disapprove
     */
-    event AllowanceDecreased(address _owner, address _spender, uint256 _amount);
+    event DecreaseApproval(address _owner, address _spender, uint256 _amount);
+
+    /**
+    * @notice MUST be emmitted when the `_owner` decreases allowance from `_sepnder` by quantity `_amount` cross-chain
+    * @param _owner the bonds owner
+    * @param _spender the account that has been allowed to spend bonds
+    * @param _amount amount of bond tokens to disapprove
+    * @param _destinationChainID The unique ID that identifies the destination Chain.
+    * @param _destinationContract The smart contract to interact with in the destination Chain in order to Deposit or Mint tokens that are transferred.
+    */
+    event CrossChainDecreaseApproval(address _owner, address _spender, uint256 _amount);
 }
