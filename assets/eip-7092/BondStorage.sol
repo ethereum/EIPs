9ba9f0dd57278a58505065ee04bdb94242f6cb56
sam say <80607135+Edoumou@users.noreply.github.com>
2023-10-07 23:23:22 +0200

    Update BondStorage

--- assets/eip-7092/BondStorage.sol
+++ assets/eip-7092/BondStorage.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: CC0-1.0
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.0;
+
+// ["0x5B38Da6a701c568545dCfcB03FcB875f56beddC4", "Republique Francaise", "info@france.fr", "France", "GOV", "AA-", "1200"]
 
 contract BondStorage {
     struct Bond {
@@ -7,27 +9,21 @@ contract BondStorage {
         string name;
         string symbol;
         address currency;
-        address currencyOfCoupon;
-        uint8 decimals;
         uint256 denomination;
         uint256 issueVolume;
         uint256 couponRate;
-        uint256 couponType;
-        uint256 couponFrequency;
         uint256 issueDate;
         uint256 maturityDate;
-        uint256 dayCountBasis;
     }
 
     struct Issuer {
+        address issuerAddress;
         string name;
         string email;
         string country;
-        string headquarters;
         string issuerType;
         string creditRating;
         uint256 carbonCredit;
-        address issuerAddress;
     }
 
     struct IssueData {
@@ -37,13 +33,12 @@ contract BondStorage {
 
     enum BondStatus {UNREGISTERED, SUBMITTED, ISSUED, REDEEMED}
 
-    mapping(string => Bond) internal _bond;
+    mapping(string => Bond) internal _bonds;
     mapping(string => Issuer) internal _issuer;
     mapping(address => uint256) internal _principals;
-    mapping(address => mapping(address => uint256)) internal _approvals;
+    mapping(address => mapping(address => uint256)) internal _allowed;
 
     string internal bondISIN;
-    string internal _countryOfIssuance;
 
     BondStatus internal _bondStatus;
     IssueData[] internal _listOfInvestors;
@@ -57,20 +52,4 @@ contract BondStorage {
     
     event BondIssued(IssueData[] _issueData, Bond _bond);
     event BondRedeemed();
-
-    function bondStatus() external view returns(BondStatus) {
-        return _bondStatus;
-    }
-
-    function listOfInvestors() external view returns(IssueData[] memory) {
-        return _listOfInvestors;
-    }
-
-    function bondInfo() public view returns(Bond memory) {
-        return _bond[bondISIN];
-    }
-    
-    function issuerInfo() public view returns(Issuer memory) {
-        return _issuer[bondISIN];
-    }
 }
