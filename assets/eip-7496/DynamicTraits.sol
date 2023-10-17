ab22c0962c53250da38f1e6ce47d3b9a531bbeed
Ryan Ghods <ryan@ryanio.com>
2023-10-17 13:30:39 -0700

    update reference implementation to latest spec

--- assets/eip-7496/DynamicTraits.sol
+++ assets/eip-7496/DynamicTraits.sol
@@ -7,142 +7,85 @@ import {IERC7496} from "./interfaces/IERC7496.sol";
 abstract contract DynamicTraits is IERC7496 {
     using EnumerableSet for EnumerableSet.Bytes32Set;
 
-    ///@notice Thrown when trying to delete a trait that has not been set
-    error TraitNotSet(uint256 tokenId, bytes32 traitKey);
-    ///@notice Thrown when trying to set a trait explicitly to the zero value hash
-    error TraitCannotBeZeroValueHash();
-    ///@notice Thrown when a new trait value is not different from the existing value
+    /// @notice Thrown when a new trait value is not different from the existing value
     error TraitValueUnchanged();
 
-    bytes32 constant ZERO_VALUE = keccak256("DYNAMIC_TRAITS_ZERO_VALUE");
-    ///@notice An enumerable set of all trait keys that have been set
+    /// @notice An enumerable set of all trait keys that have been set
     EnumerableSet.Bytes32Set internal _traitKeys;
-    ///@notice A mapping of token ID to a mapping of trait key to trait value
-    mapping(uint256 tokenId => mapping(bytes32 traitKey => bytes32 traitValue)) internal _traits;
-    ///@notice An offchain string URI that points to a JSON file containing trait labels
-    string internal _traitLabelsURI;
 
-    function setTrait(bytes32 traitKey, uint256 tokenId, bytes32 trait) external virtual;
-    function deleteTrait(bytes32 traitKey, uint256 tokenId) external virtual;
+    /// @notice A mapping of token ID to a mapping of trait key to trait value
+    mapping(uint256 tokenId => mapping(bytes32 traitKey => bytes32 traitValue)) internal _traits;
+
+    /// @notice An offchain string URI that points to a JSON file containing trait metadata
+    string internal _traitMetadataURI;
 
     /**
-     * @notice Get the value of a trait for a given token ID. Reverts if the trait is not set.
-     * @param traitKey The trait key to get the value of
+     * @notice Get the value of a trait for a given token ID.
      * @param tokenId The token ID to get the trait value for
+     * @param traitKey The trait key to get the value of
      */
-    function getTraitValue(bytes32 traitKey, uint256 tokenId) public view virtual returns (bytes32) {
-        bytes32 value = _traits[tokenId][traitKey];
-        // Revert if the trait is not set
-        if (value == bytes32(0)) {
-            revert TraitNotSet(tokenId, traitKey);
-        } else if (value == ZERO_VALUE) {
-            // check for zero value hash; return 0 if so
-            return bytes32(0);
-        } else {
-            // otherwise return normal value
-            return value;
-        }
+    function getTraitValue(uint256 tokenId, bytes32 traitKey) public view virtual returns (bytes32 traitValue) {
+        traitValue = _traits[tokenId][traitKey];
     }
 
     /**
-     * @notice Get the values of a trait for a given list of token IDs. Reverts if the trait is not set on any single token.
-     * @param traitKey The trait key to get the value of
-     * @param tokenIds The token IDs to get the trait values for
+     * @notice Get the values of traits for a given token ID.
+     * @param tokenId The token ID to get the trait values for
+     * @param traitKeys The trait keys to get the values of
      */
-    function getTraitValues(bytes32 traitKey, uint256[] calldata tokenIds)
-        external
+    function getTraitValues(uint256 tokenId, bytes32[] calldata traitKeys)
+        public
         view
         virtual
         returns (bytes32[] memory traitValues)
     {
-        uint256 length = tokenIds.length;
-        bytes32[] memory result = new bytes32[](length);
-        for (uint256 i = 0; i < length; i++) {
-            uint256 tokenId = tokenIds[i];
-            result[i] = getTraitValue(traitKey, tokenId);
+        uint256 length = traitKeys.length;
+        traitValues = new bytes32[](length);
+        for (uint256 i = 0; i < length;) {
+            bytes32 traitKey = traitKeys[i];
+            traitValues[i] = getTraitValue(tokenId, traitKey);
+            unchecked {
+                ++i;
+            }
         }
-        return result;
     }
 
     /**
-     * @notice Get the total number of trait keys that have been set
+     * @notice Get the URI for the trait metadata
      */
-    function getTotalTraitKeys() external view virtual returns (uint256) {
-        return _traitKeys.length();
-    }
-
-    /**
-     * @notice Get the trait key at a given index
-     * @param index The index of the trait key to get
-     */
-    function getTraitKeyAt(uint256 index) external view virtual returns (bytes32 traitKey) {
-        return _traitKeys.at(index);
-    }
-
-    /**
-     * @notice Get the trait keys that have been set. May revert if there are too many trait keys.
-     */
-    function getTraitKeys() external view virtual returns (bytes32[] memory traitKeys) {
-        return _traitKeys._inner._values;
-    }
-
-    /**
-     * @notice Get the URI for the trait labels
-     */
-    function getTraitLabelsURI() external view virtual returns (string memory labelsURI) {
-        return _traitLabelsURI;
+    function getTraitMetadataURI() external view virtual returns (string memory labelsURI) {
+        return _traitMetadataURI;
     }
 
     /**
      * @notice Set the value of a trait for a given token ID. If newTrait is bytes32(0), sets the zero value hash.
      *         Reverts if the trait value is the zero value hash.
-     * @param traitKey The trait key to set the value of
      * @param tokenId The token ID to set the trait value for
-     * @param newTrait The new trait value to set
+     * @param traitKey The trait key to set the value of
+     * @param newValue The new trait value to set
      */
-    function _setTrait(bytes32 traitKey, uint256 tokenId, bytes32 newTrait) internal {
+    function _setTrait(uint256 tokenId, bytes32 traitKey, bytes32 newValue) internal {
         bytes32 existingValue = _traits[tokenId][traitKey];
 
-        if (newTrait == bytes32(0)) {
-            newTrait = ZERO_VALUE;
-        } else if (newTrait == ZERO_VALUE) {
-            revert InvalidTraitValue(traitKey, newTrait);
-        }
-
-        if (existingValue == newTrait) {
+        if (existingValue == newValue) {
             revert TraitValueUnchanged();
         }
 
         // no-op if exists
         _traitKeys.add(traitKey);
 
-        _traits[tokenId][traitKey] = newTrait;
+        _traits[tokenId][traitKey] = newValue;
 
-        emit TraitUpdated(traitKey, tokenId, newTrait);
+        emit TraitUpdated(traitKey, tokenId, newValue);
     }
 
     /**
-     * @notice Delete the value of a trait for a given token ID.
-     * @param traitKey The trait key to delete the value of
-     * @param tokenId The token ID to delete the trait value for
-     */
-    function _deleteTrait(bytes32 traitKey, uint256 tokenId) internal {
-        bytes32 existingValue = _traits[tokenId][traitKey];
-        if (existingValue == bytes32(0)) {
-            revert TraitValueUnchanged();
-        }
-
-        _traits[tokenId][traitKey] = bytes32(0);
-        emit TraitUpdated(traitKey, tokenId, bytes32(0));
-    }
-
-    /**
-     * @notice Set the URI for the trait labels
+     * @notice Set the URI for the trait metadata
      * @param uri The new URI to set
      */
-    function _setTraitLabelsURI(string calldata uri) internal virtual {
-        _traitLabelsURI = uri;
-        emit TraitLabelsURIUpdated(uri);
+    function _setTraitMetadataURI(string calldata uri) internal virtual {
+        _traitMetadataURI = uri;
+        emit TraitMetadataURIUpdated();
     }
 
     function supportsInterface(bytes4 interfaceId) public view virtual returns (bool) {
---
+++ assets/eip-7496/ERC721DynamicTraits.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {ERC721} from "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import {Ownable} from "openzeppelin-contracts/access/Ownable.sol";
+import {DynamicTraits} from "./DynamicTraits.sol";
+
+contract ERC721DynamicTraits is DynamicTraits, Ownable, ERC721 {
+    constructor() Ownable(msg.sender) ERC721("ERC721DynamicTraits", "ERC721DT") {
+        _traitMetadataURI = "https://example.com";
+    }
+
+    function setTrait(uint256 tokenId, bytes32 traitKey, bytes32 value) external virtual override onlyOwner {
+        // Revert if the token doesn't exist.
+        _requireOwned(tokenId);
+
+        _setTrait(tokenId, traitKey, value);
+    }
+
+    function getTraitValue(uint256 tokenId, bytes32 traitKey)
+        public
+        view
+        virtual
+        override
+        returns (bytes32 traitValue)
+    {
+        // Revert if the token doesn't exist.
+        _requireOwned(tokenId);
+
+        return DynamicTraits.getTraitValue(tokenId, traitKey);
+    }
+
+    function getTraitValues(uint256 tokenId, bytes32[] calldata traitKeys)
+        public
+        view
+        virtual
+        override
+        returns (bytes32[] memory traitValues)
+    {
+        // Revert if the token doesn't exist.
+        _requireOwned(tokenId);
+
+        return DynamicTraits.getTraitValues(tokenId, traitKeys);
+    }
+
+    function setTraitMetadataURI(string calldata uri) external onlyOwner {
+        _setTraitMetadataURI(uri);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, DynamicTraits) returns (bool) {
+        return ERC721.supportsInterface(interfaceId) || DynamicTraits.supportsInterface(interfaceId);
+    }
+}
