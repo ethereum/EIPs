589eb304b8250722bfa6a20f1a6e3a626ac07d81
Etan Kissling <etan@status.im>
2024-04-15 18:59:40 +0200

    Update EIP-7495: Compact serialization for `Variant[S]`
    Merged by EIP-Bot.

--- assets/eip-7495/stable_container.py
+++ assets/eip-7495/stable_container.py
@@ -1,5 +1,5 @@
 import io
-from typing import BinaryIO, Dict, List as PyList, Optional, TypeVar, Type, Union as PyUnion, \
+from typing import BinaryIO, Dict, List as PyList, Optional, Tuple, TypeVar, Type, Union as PyUnion, \
     get_args, get_origin
 from textwrap import indent
 from remerkleable.bitfields import Bitvector
@@ -12,8 +12,18 @@ from remerkleable.tree import NavigationError, Node, PairNode, \
 N = TypeVar('N')
 S = TypeVar('S', bound="ComplexView")
 
+
+def all_fields(cls) -> Dict[str, Tuple[Type[View], bool]]:
+    fields = {}
+    for k, v in cls.__annotations__.items():
+        fopt = get_origin(v) == PyUnion and type(None) in get_args(v)
+        ftyp = get_args(v)[0] if fopt else v
+        fields[k] = (ftyp, fopt)
+    return fields
+
+
 class StableContainer(ComplexView):
-    _field_indices: Dict[str, tuple[int, Type[View], bool]]
+    _field_indices: Dict[str, Tuple[int, Type[View], bool]]
     __slots__ = '_field_indices'
 
     def __new__(cls, backing: Optional[Node] = None, hook: Optional[ViewHook] = None, **kwargs):
@@ -71,13 +81,8 @@ class StableContainer(ComplexView):
         return StableContainerView
 
     @classmethod
-    def fields(cls) -> Dict[str, tuple[Type[View], bool]]:
-        fields = {}
-        for k, v in cls.__annotations__.items():
-            fopt = get_origin(v) == PyUnion and type(None) in get_args(v)
-            ftyp = get_args(v)[0] if fopt else v
-            fields[k] = (ftyp, fopt)
-        return fields
+    def fields(cls) -> Dict[str, Tuple[Type[View], bool]]:
+        return all_fields(cls)
 
     @classmethod
     def is_fixed_byte_length(cls) -> bool:
@@ -247,7 +252,10 @@ class StableContainer(ComplexView):
 
         return num_prefix_bytes + num_data_bytes
 
+
 class Variant(ComplexView):
+    _o: int
+
     def __new__(cls, backing: Optional[Node] = None, hook: Optional[ViewHook] = None, **kwargs):
         if backing is not None:
             if len(kwargs) != 0:
@@ -268,6 +276,13 @@ class Variant(ComplexView):
         value = cls.S(backing, hook, **kwargs)
         return cls(backing=value.get_backing())
 
+    def __init_subclass__(cls, *args, **kwargs):
+        super().__init_subclass__(*args, **kwargs)
+        cls._o = 0
+        for _, (_, fopt) in cls.fields().items():
+            if fopt:
+                cls._o += 1
+
     def __class_getitem__(cls, s) -> Type["Variant"]:
         if not issubclass(s, StableContainer):
             raise Exception(f"invalid variant container: {s}")
@@ -275,12 +290,64 @@ class Variant(ComplexView):
         class VariantView(Variant, s):
             S = s
 
-            @classmethod
-            def fields(cls) -> Dict[str, tuple[Type[View], bool]]:
-                return s.fields()
-
         VariantView.__name__ = VariantView.type_repr()
         return VariantView
+
+    @classmethod
+    def fields(cls) -> Dict[str, Tuple[Type[View], bool]]:
+        return all_fields(cls)
+
+    @classmethod
+    def is_fixed_byte_length(cls) -> bool:
+        if cls._o > 0:
+            return False
+        for _, (ftyp, _) in cls.fields().items():
+            if not ftyp.is_fixed_byte_length():
+                return False
+        return True
+
+    @classmethod
+    def type_byte_length(cls) -> int:
+        if cls.is_fixed_byte_length():
+            return cls.min_byte_length()
+        else:
+            raise Exception("dynamic length variant does not have a fixed byte length")
+
+    @classmethod
+    def min_byte_length(cls) -> int:
+        total = Bitvector[cls._o].type_byte_length() if cls._o > 0 else 0
+        for _, (ftyp, fopt) in cls.fields().items():
+            if fopt:
+                continue
+            if not ftyp.is_fixed_byte_length():
+                total += OFFSET_BYTE_LENGTH
+            total += ftyp.min_byte_length()
+        return total
+
+    @classmethod
+    def max_byte_length(cls) -> int:
+        total = Bitvector[cls._o].type_byte_length() if cls._o > 0 else 0
+        for _, (ftyp, _) in cls.fields().items():
+            if not ftyp.is_fixed_byte_length():
+                total += OFFSET_BYTE_LENGTH
+            total += ftyp.max_byte_length()
+        return total
+
+    def active_fields(self) -> Bitvector:
+        active_fields_node = super().get_backing().get_right()
+        return Bitvector[self.__class__.S.N].view_from_backing(active_fields_node)
+
+    def optional_fields(self) -> Bitvector:
+        assert self.__class__._o > 0
+        active_fields = self.active_fields()
+        optional_fields = Bitvector[self.__class__._o]()
+        oindex = 0
+        for fkey, (_, fopt) in self.__class__.fields().items():
+            if fopt:
+                (findex, _, _) = self.__class__.S._field_indices[fkey]
+                optional_fields.set(oindex, active_fields.get(findex))
+                oindex += 1
+        return optional_fields
 
     @classmethod
     def type_repr(cls) -> str:
@@ -288,8 +355,92 @@ class Variant(ComplexView):
 
     @classmethod
     def deserialize(cls: Type[S], stream: BinaryIO, scope: int) -> S:
-        value = cls.S.deserialize(stream, scope)
-        return cls(backing=value.get_backing())
+        if cls._o > 0:
+            num_prefix_bytes = Bitvector[cls._o].type_byte_length()
+            if scope < num_prefix_bytes:
+                raise ValueError("scope too small, cannot read Variant optional fields")
+            optional_fields = Bitvector[cls._o].deserialize(stream, num_prefix_bytes)
+            scope = scope - num_prefix_bytes
+
+        field_values: Dict[str, Optional[View]] = {}
+        dyn_fields: PyList[FieldOffset] = []
+        fixed_size = 0
+        oindex = 0
+        for fkey, (ftyp, fopt) in cls.fields().items():
+            if fopt:
+                have_field = optional_fields.get(oindex)
+                oindex += 1
+                if not have_field:
+                    field_values[fkey] = None
+                    continue
+            if ftyp.is_fixed_byte_length():
+                fsize = ftyp.type_byte_length()
+                field_values[fkey] = ftyp.deserialize(stream, fsize)
+                fixed_size += fsize
+            else:
+                dyn_fields.append(FieldOffset(
+                    key=fkey, typ=ftyp, offset=int(decode_offset(stream))))
+                fixed_size += OFFSET_BYTE_LENGTH
+        assert oindex == cls._o
+        if len(dyn_fields) > 0:
+            if dyn_fields[0].offset < fixed_size:
+                raise Exception(f"first offset {dyn_fields[0].offset} is "
+                                f"smaller than expected fixed size {fixed_size}")
+            for i, (fkey, ftyp, foffset) in enumerate(dyn_fields):
+                next_offset = dyn_fields[i + 1].offset if i + 1 < len(dyn_fields) else scope
+                if foffset > next_offset:
+                    raise Exception(f"offset {i} is invalid: {foffset} "
+                                    f"larger than next offset {next_offset}")
+                fsize = next_offset - foffset
+                f_min_size, f_max_size = ftyp.min_byte_length(), ftyp.max_byte_length()
+                if not (f_min_size <= fsize <= f_max_size):
+                    raise Exception(f"offset {i} is invalid, size out of bounds: "
+                                    f"{foffset}, next {next_offset}, implied size: {fsize}, "
+                                    f"size bounds: [{f_min_size}, {f_max_size}]")
+                field_values[fkey] = ftyp.deserialize(stream, fsize)
+
+        return cls(**field_values)  # type: ignore
+
+    def serialize(self, stream: BinaryIO) -> int:
+        if self.__class__._o > 0:
+            optional_fields = self.optional_fields()
+            num_prefix_bytes = optional_fields.serialize(stream)
+        else:
+            num_prefix_bytes = 0
+
+        num_data_bytes = 0
+        oindex = 0
+        for _, (ftyp, fopt) in self.__class__.fields().items():
+            if fopt:
+                have_field = optional_fields.get(oindex)
+                oindex += 1
+                if not have_field:
+                    continue
+            if ftyp.is_fixed_byte_length():
+                num_data_bytes += ftyp.type_byte_length()
+            else:
+                num_data_bytes += OFFSET_BYTE_LENGTH
+        assert oindex == self.__class__._o
+
+        temp_dyn_stream = io.BytesIO()
+        data = super().get_backing().get_left()
+        active_fields = self.active_fields()
+        for fkey, (ftyp, _) in self.__class__.fields().items():
+            (findex, _, _) = self.__class__.S._field_indices[fkey]
+            if not active_fields.get(findex):
+                continue
+            fnode = data.getter(2**get_depth(self.__class__.N) + findex)
+            v = ftyp.view_from_backing(fnode)
+            if ftyp.is_fixed_byte_length():
+                v.serialize(stream)
+            else:
+                encode_offset(stream, num_data_bytes)
+                num_data_bytes += v.serialize(temp_dyn_stream)  # type: ignore
+        temp_dyn_stream.seek(0)
+        stream.write(temp_dyn_stream.read(num_data_bytes))
+
+        return num_prefix_bytes + num_data_bytes
+
 
 class OneOf(ComplexView):
     def __class_getitem__(cls, s) -> Type["OneOf"]:
