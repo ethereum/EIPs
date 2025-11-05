7e349f38b72d3ccfdb56f66f72277a21777f50b0
Doryu <anna.shuraeva16@gmail.com>
2025-11-05 20:11:56 +0100

    Update eth2_ws_calc.py

--- assets/eip-6110/eth2_ws_calc.py
+++ assets/eip-6110/eth2_ws_calc.py
@@ -4,11 +4,10 @@ This script calculates the Eth2 Weak Subjectivity period as defined by eth2.0-sp
 
 from eth2spec.phase0.mainnet import (
     uint64, Ether,
-    ETH_TO_GWEI, 
-    MAX_DEPOSITS, 
-    MAX_EFFECTIVE_BALANCE,  
-    SLOTS_PER_EPOCH, 
-    config, 
+    ETH_TO_GWEI,
+    MAX_EFFECTIVE_BALANCE,
+    SLOTS_PER_EPOCH,
+    config,
 )
 
 MIN_VALIDATOR_WITHDRAWABILITY_DELAY = config.MIN_VALIDATOR_WITHDRAWABILITY_DELAY
