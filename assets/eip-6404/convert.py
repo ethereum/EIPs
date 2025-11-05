e3addc49b332e279d9038067192daf81a4c76562
Etan Kissling <etan@nimbus.team>
2025-11-05 14:00:30 +0100

    Update EIP-6404: Move union layer into payload

--- assets/eip-6404/convert.py
+++ assets/eip-6404/convert.py
@@ -11,33 +11,33 @@ def upgrade_rlp_transaction_to_ssz(tx_bytes: bytes):
         def upgrade_authorization(auth: SetCodeRlpAuthorization):
             if auth.chain_id != 0:
                 return RlpSetCodeAuthorization(
-                    selector=0x02,
-                    data=RlpBasicAuthorization(
-                        payload=RlpBasicAuthorizationPayload(
+                    payload=RlpSetCodeAuthorizationPayload(
+                        selector=0x02,
+                        data=RlpBasicAuthorizationPayload(
                             magic=RlpTxType.SET_CODE_MAGIC,
                             chain_id=auth.chain_id,
                             address=ExecutionAddress(auth.address),
                             nonce=auth.nonce,
                         ),
-                        signature=secp256k1_pack(auth.r, auth.s, auth.y_parity),
                     ),
+                    signature=secp256k1_pack(auth.r, auth.s, auth.y_parity),
                 )
             return RlpSetCodeAuthorization(
-                selector=0x01,
-                data=RlpReplayableBasicAuthorization(
-                    payload=RlpReplayableBasicAuthorizationPayload(
+                payload=RlpSetCodeAuthorizationPayload(
+                    selector=0x01,
+                    data=RlpReplayableBasicAuthorizationPayload(
                         magic=RlpTxType.SET_CODE_MAGIC,
                         address=ExecutionAddress(auth.address),
                         nonce=auth.nonce,
                     ),
-                    signature=secp256k1_pack(auth.r, auth.s, auth.y_parity),
                 ),
+                signature=secp256k1_pack(auth.r, auth.s, auth.y_parity),
             )
 
         return Transaction(
-            selector=0x0a,
-            data=RlpSetCodeTransaction(
-                payload=RlpSetCodeTransactionPayload(
+            payload=TransactionPayload(
+                selector=0x0a,
+                data=RlpSetCodeTransactionPayload(
                     type_=RlpTxType.SET_CODE,
                     chain_id=tx.chain_id,
                     nonce=tx.nonce,
@@ -60,16 +60,16 @@ def upgrade_rlp_transaction_to_ssz(tx_bytes: bytes):
                         for auth in tx.authorization_list
                     ],
                 ),
-                signature=secp256k1_pack(tx.r, tx.s, tx.y_parity),
             ),
+            signature=secp256k1_pack(tx.r, tx.s, tx.y_parity),
         )
 
     if type_ == 0x03:  # EIP-4844
         tx = decode(tx_bytes[1:], BlobRlpTransaction)
         return Transaction(
-            selector=0x09,
-            data=RlpBlobTransaction(
-                payload=RlpBlobTransactionPayload(
+            payload=TransactionPayload(
+                selector=0x09,
+                data=RlpBlobTransactionPayload(
                     type_=RlpTxType.BLOB,
                     chain_id=tx.chain_id,
                     nonce=tx.nonce,
@@ -91,17 +91,17 @@ def upgrade_rlp_transaction_to_ssz(tx_bytes: bytes):
                     ),
                     blob_versioned_hashes=tx.blob_versioned_hashes,
                 ),
-                signature=secp256k1_pack(tx.r, tx.s, tx.y_parity),
             ),
+            signature=secp256k1_pack(tx.r, tx.s, tx.y_parity),
         )
 
     if type_ == 0x02:  # EIP-1559
         tx = decode(tx_bytes[1:], FeeMarketRlpTransaction)
         if len(tx.to) == 0:
             return Transaction(
-                selector=0x08,
-                data=RlpCreateTransaction(
-                    payload=RlpCreateTransactionPayload(
+                payload=TransactionPayload(
+                    selector=0x08,
+                    data=RlpCreateTransactionPayload(
                         type_=RlpTxType.FEE_MARKET,
                         chain_id=tx.chain_id,
                         nonce=tx.nonce,
@@ -119,13 +119,13 @@ def upgrade_rlp_transaction_to_ssz(tx_bytes: bytes):
                             regular=tx.max_priority_fee_per_gas,
                         ),
                     ),
-                    signature=secp256k1_pack(tx.r, tx.s, tx.y_parity),
                 ),
+                signature=secp256k1_pack(tx.r, tx.s, tx.y_parity),
             )
         return Transaction(
-            selector=0x07,
-            data=RlpBasicTransaction(
-                payload=RlpBasicTransactionPayload(
+            payload=TransactionPayload(
+                selector=0x07,
+                data=RlpBasicTransactionPayload(
                     type_=RlpTxType.FEE_MARKET,
                     chain_id=tx.chain_id,
                     nonce=tx.nonce,
@@ -144,17 +144,17 @@ def upgrade_rlp_transaction_to_ssz(tx_bytes: bytes):
                         regular=tx.max_priority_fee_per_gas,
                     ),
                 ),
-                signature=secp256k1_pack(tx.r, tx.s, tx.y_parity),
             ),
+            signature=secp256k1_pack(tx.r, tx.s, tx.y_parity),
         )
 
     if type_ == 0x01:  # EIP-2930
         tx = decode(tx_bytes[1:], AccessListRlpTransaction)
         if len(tx.to) == 0:
             return Transaction(
-                selector=0x06,
-                data=RlpAccessListCreateTransaction(
-                    payload=RlpAccessListCreateTransactionPayload(
+                payload=TransactionPayload(
+                    selector=0x06,
+                    data=RlpAccessListCreateTransactionPayload(
                         type_=RlpTxType.ACCESS_LIST,
                         chain_id=tx.chain_id,
                         nonce=tx.nonce,
@@ -169,13 +169,13 @@ def upgrade_rlp_transaction_to_ssz(tx_bytes: bytes):
                             storage_keys=access_tuple[1]
                         ) for access_tuple in tx.access_list],
                     ),
-                    signature=secp256k1_pack(tx.r, tx.s, tx.y_parity),
                 ),
+                signature=secp256k1_pack(tx.r, tx.s, tx.y_parity),
             )
         return Transaction(
-            selector=0x05,
-            data=RlpAccessListBasicTransaction(
-                payload=RlpAccessListBasicTransactionPayload(
+            payload=TransactionPayload(
+                selector=0x05,
+                data=RlpAccessListBasicTransactionPayload(
                     type_=RlpTxType.ACCESS_LIST,
                     chain_id=tx.chain_id,
                     nonce=tx.nonce,
@@ -191,8 +191,8 @@ def upgrade_rlp_transaction_to_ssz(tx_bytes: bytes):
                         storage_keys=access_tuple[1]
                     ) for access_tuple in tx.access_list],
                 ),
-                signature=secp256k1_pack(tx.r, tx.s, tx.y_parity),
             ),
+            signature=secp256k1_pack(tx.r, tx.s, tx.y_parity),
         )
 
     if 0xc0 <= type_ <= 0xfe:  # Legacy
@@ -200,9 +200,9 @@ def upgrade_rlp_transaction_to_ssz(tx_bytes: bytes):
         if tx.v not in (27, 28):
             if len(tx.to) == 0:
                 return Transaction(
-                    selector=0x04,
-                    data=RlpLegacyCreateTransaction(
-                        payload=RlpLegacyCreateTransactionPayload(
+                    payload=TransactionPayload(
+                        selector=0x04,
+                        data=RlpLegacyCreateTransactionPayload(
                             type_=RlpTxType.LEGACY,
                             chain_id=(tx.v - 35) >> 1,
                             nonce=tx.nonce,
@@ -213,13 +213,13 @@ def upgrade_rlp_transaction_to_ssz(tx_bytes: bytes):
                             value=tx.value,
                             input_=tx.data,
                         ),
-                        signature=secp256k1_pack(tx.r, tx.s, y_parity=(tx.v & 0x1) == 0),
                     ),
+                    signature=secp256k1_pack(tx.r, tx.s, y_parity=(tx.v & 0x1) == 0),
                 )
             return Transaction(
-                selector=0x03,
-                data=RlpLegacyBasicTransaction(
-                    payload=RlpLegacyBasicTransactionPayload(
+                payload=TransactionPayload(
+                    selector=0x03,
+                    data=RlpLegacyBasicTransactionPayload(
                         type_=RlpTxType.LEGACY,
                         chain_id=(tx.v - 35) >> 1,
                         nonce=tx.nonce,
@@ -231,14 +231,14 @@ def upgrade_rlp_transaction_to_ssz(tx_bytes: bytes):
                         value=tx.value,
                         input_=tx.data,
                     ),
-                    signature=secp256k1_pack(tx.r, tx.s, y_parity=(tx.v & 0x1) == 0),
                 ),
+                signature=secp256k1_pack(tx.r, tx.s, y_parity=(tx.v & 0x1) == 0),
             )
         if len(tx.to) == 0:
             return Transaction(
-                selector=0x02,
-                data=RlpLegacyReplayableCreateTransaction(
-                    payload=RlpLegacyReplayableCreateTransactionPayload(
+                payload=TransactionPayload(
+                    selector=0x02,
+                    data=RlpLegacyReplayableCreateTransactionPayload(
                         type_=RlpTxType.LEGACY,
                         nonce=tx.nonce,
                         max_fees_per_gas=BasicFeesPerGas(
@@ -248,13 +248,13 @@ def upgrade_rlp_transaction_to_ssz(tx_bytes: bytes):
                         value=tx.value,
                         input_=tx.data,
                     ),
-                    signature=secp256k1_pack(tx.r, tx.s, y_parity=(tx.v & 0x1) == 0),
                 ),
+                signature=secp256k1_pack(tx.r, tx.s, y_parity=(tx.v & 0x1) == 0),
             )
         return Transaction(
-            selector=0x01,
-            data=RlpLegacyReplayableBasicTransaction(
-                payload=RlpLegacyReplayableBasicTransactionPayload(
+            payload=TransactionPayload(
+                selector=0x01,
+                data=RlpLegacyReplayableBasicTransactionPayload(
                     type_=RlpTxType.LEGACY,
                     nonce=tx.nonce,
                     max_fees_per_gas=BasicFeesPerGas(
@@ -265,8 +265,8 @@ def upgrade_rlp_transaction_to_ssz(tx_bytes: bytes):
                     value=tx.value,
                     input_=tx.data,
                 ),
-                signature=secp256k1_pack(tx.r, tx.s, y_parity=(tx.v & 0x1) == 0),
             ),
+            signature=secp256k1_pack(tx.r, tx.s, y_parity=(tx.v & 0x1) == 0),
         )
 
     assert False
