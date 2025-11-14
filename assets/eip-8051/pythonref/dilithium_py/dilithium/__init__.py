afb839156afcc1ef8711f906d6f107c666827c2c
Simon Masson <simon.masson@protonmail.com>
2025-11-14 15:52:34 -0400

    Remove python code, provide KAT vectors

--- assets/eip-8051/pythonref/dilithium_py/dilithium/__init__.py
+++
@@ -1,8 +0,0 @@
-from .default_parameters import Dilithium2, Dilithium3, Dilithium5, ZKDilithiumBB, ZKDilithiumKB
-
-__all__ = [
-    # default instances
-    "Dilithium2", "Dilithium3", "Dilithium5",
-    # ZK instances
-    "ZKDilithiumBB", "ZKDilithiumKB"
-]
