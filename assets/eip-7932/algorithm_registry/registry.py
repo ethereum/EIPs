63a97b9de1882cdb5c30c9c044ff1a5fb4af844c
SirSpudlington <73645805+SirSpudlington@users.noreply.github.com>
2025-12-13 21:52:44 +0000

    Rewrite the EIP-7932 interface to be more PQ-friendly

--- assets/eip-7932/algorithm_registry/registry.py
+++ assets/eip-7932/algorithm_registry/registry.py
@@ -1,7 +1,7 @@
 from typing import Callable, Dict
-from eth_typing import Hash32
 from remerkleable.byte_arrays import ByteVector
-from remerkleable.basic import uint8, uint256, uint
+from remerkleable.basic import uint8, uint32, uint64, uint256
+from eth_hash.auto import keccak
 
 from secp256k1 import PublicKey, ECDSA
 
@@ -9,11 +9,12 @@ from secp256k1 import PublicKey, ECDSA
 # Registry setup
 
 
-class AlgorithmEntry:
+class AlgorithmEntry():
     ALG_TYPE: uint8
-    GAS_PENALTY: uint
-    verify: Callable[[bytes, Hash32], bytes]
-
+    SIZE: uint32
+    gas_cost: Callable[[bytes], uint64]
+    validate: Callable[[bytes], None]
+    verify: Callable[[bytes, bytes], bytes]
 
 algorithm_registry: Dict[uint8, AlgorithmEntry] = {}
 
@@ -21,18 +22,15 @@ algorithm_registry: Dict[uint8, AlgorithmEntry] = {}
 
 SECP256K1_SIGNATURE_SIZE = 65
 
-
-def secp256k1_unpack(
-    signature: ByteVector[SECP256K1_SIGNATURE_SIZE],
-) -> tuple[uint256, uint256, uint8]:
-    r = uint256.from_bytes(signature[0:32], "big")
-    s = uint256.from_bytes(signature[32:64], "big")
+def secp256k1_unpack(signature: ByteVector[SECP256K1_SIGNATURE_SIZE]) -> tuple[uint256, uint256, uint8]:
+    r = uint256.from_bytes(signature[0:32], 'big')
+    s = uint256.from_bytes(signature[32:64], 'big')
     y_parity = signature[64]
-    return (r, s, uint8(y_parity))
+    return (r, s, y_parity)
 
 
 def secp256k1_validate(signature: ByteVector[SECP256K1_SIGNATURE_SIZE]):
-    SECP256K1N = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141
+    SECP256K1N = 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141
     r, s, y_parity = secp256k1_unpack(signature)
     assert 0 < r < SECP256K1N
     assert 0 < s <= SECP256K1N // 2
@@ -40,18 +38,28 @@ def secp256k1_validate(signature: ByteVector[SECP256K1_SIGNATURE_SIZE]):
 
 
 class Secp256k1(AlgorithmEntry):
-    ALG_TYPE = uint8(0xFF)
-    GAS_PENALTY = uint256(0)
+    ALG_TYPE = 0xFF
+    SIZE = 66
 
-    def verify(signature_info: bytes, payload_hash: Hash32) -> bytes:
-        assert len(signature_info) == (SECP256K1_SIGNATURE_SIZE + 1)
-        secp256k1_validate(signature_info[1:])
+    def gas_cost(signing_data: bytes) -> uint64:
+        # This is an adaptation from the KECCAK256 opcode
+        if len(signing_data) == 32:
+            return uint64(0)
+        else:
+            minimum_word_size = (len(signing_data) + 31) // 32
+            return uint64(30 + (6 * minimum_word_size))
+
+    def validate(signature: bytes):
+        secp256k1_validate(signature[1:])
+
+    def verify(signature: bytes, signing_data: bytes) -> bytes:
+        # Another compatibility shim to ensure passing a 32 byte hash still works.
+        if len(signing_data) != 32:
+            signing_data = bytes(keccak(signing_data))
 
         ecdsa = ECDSA()
-        recover_sig = ecdsa.ecdsa_recoverable_deserialize(
-            signature_info[1:65], signature_info[65]
-        )
-        public_key = PublicKey(ecdsa.ecdsa_recover(payload_hash, recover_sig, raw=True))
+        recover_sig = ecdsa.ecdsa_recoverable_deserialize(signature[1:65], signature[65])
+        public_key = PublicKey(ecdsa.ecdsa_recover(signing_data, recover_sig, raw=True))
         uncompressed = public_key.serialize(compressed=False)
         return uncompressed
 
